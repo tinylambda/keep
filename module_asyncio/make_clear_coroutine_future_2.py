33b42f2afd4e5e12fd9f8a9dbc72b388ea03fafb
Felix <tinylambda@gmail.com>
2020-03-04 11:19:31 +0800

    Add more asyncio

---
+++ module_asyncio/make_clear_coroutine_future_2.py
@@ -0,0 +1,24 @@
+# https://stackoverflow.com/questions/34753401/difference-between-coroutine-and-future-task-in-python-3-5
+
+import asyncio
+import time
+
+
+async def p(word):
+    print(f'{time.time()} - {word}')
+
+
+async def main():
+    loop = asyncio.get_event_loop()
+    coro = p('await')
+    task2 = loop.create_task(p('create_task'))  # Scheduled to next iteration
+    await asyncio.sleep(1)  # loop got control, and runs task2
+    await coro
+    await task2
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
+
