35467a2fc452ad7398dd6c495083c990b13b532e
Felix Pan <tinylambda@gmail.com>
2020-03-05 00:39:36 +0800

    Add more asyncio
    Add more asyncio

---
+++ module_asyncio/asyncio_gather.py
@@ -0,0 +1,32 @@
+# If the background phases are well-defined, and only the results of those phases matter, then gather() may be more
+# useful for waiting for multiple operations
+
+import asyncio
+
+
+async def phase1():
+    print('in phase1')
+    await asyncio.sleep(2)
+    print('done with phase1')
+    return 'phase1 result'
+
+
+async def phase2():
+    print('in phase2')
+    await asyncio.sleep(1)
+    print('done with phase2')
+    return 'phase2 result'
+
+
+async def main():
+    print('starting main')
+    print('waiting for phases to complete')
+    results = await asyncio.gather(phase1(), phase2())
+    print('results: {!r}'.format(results))
+
+
+event_loop = asyncio.get_event_loop()
+try:
+    event_loop.run_until_complete(main())
+finally:
+    event_loop.close()
