743c46e1792b15dade8164a4e6a9ca482b0390c7
Felix <tinylambda@gmail.com>
2020-04-16 16:29:41 +0800

    Add collections.namedtuple

---
+++ module_collections/collections_namedtuple_asdict.py
@@ -0,0 +1,12 @@
+import collections
+
+
+Person = collections.namedtuple(
+    'Person', 'name age'
+)
+
+bob = Person(name='Bob', age=30)
+print('Representation: ', bob)
+print('As Dictionary: ', bob._asdict())
+
+
---
+++ module_collections/collections_namedtuple_person.py
@@ -0,0 +1,20 @@
+import collections
+
+
+Person = collections.namedtuple('Person', 'name age')
+
+bob = Person(name='Bob', age=30)
+print('\nRepresentation: ', bob)
+
+jane = Person(name='Jane', age=29)
+print('\nField by name: ', jane.name)
+
+print('\nFields by index: ')
+for p in [bob, jane]:
+    print('{} is {} years old'.format(*p))
+
+#  it is possible to access the fields of the namedtuple by name using dotted notation (obj.attr) as well as by
+#  using the positional indexes of standard tuples.
+
+
+
