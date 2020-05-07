33e863009bbe0c16aa91cc7b0f5a8ea6d5f0a16b
Felix Pan <tinylambda@gmail.com>
2020-05-07 16:27:42 +0800

    Add copy module

---
+++ module_copy/copy_recursion.py
@@ -0,0 +1,42 @@
+import copy
+
+
+class Graph:
+    def __init__(self, name, connections):
+        self.name = name
+        self.connections = connections
+
+    def add_connection(self, other):
+        self.connections.append(other)
+
+    def __repr__(self):
+        return 'Graph(name={}, id={})'.format(self.name, id(self))
+
+    def __deepcopy__(self, memodict):
+        print('\nCalling __deepcopy__ for {!r}'.format(self))
+        if self in memodict:
+            existing = memodict.get(self)
+            print('Already copied to {!r}'.format(existing))
+            return existing
+        print('Memo dictionary: ')
+        if memodict:
+            for k, v in memodict.items():
+                print('{}: {}'.format(k, v))
+        else:
+            print('(empty)')
+        dup = Graph(copy.deepcopy(self.name, memodict), [])
+        print('Copying to new object {}'.format(dup))
+        memodict[self] = dup  # Cache for reuse
+        for c in self.connections:
+            dup.add_connection(copy.deepcopy(c, memodict))
+        return dup
+
+
+root = Graph('root', [])
+a = Graph('a', [root])
+b = Graph('b', [a, root])
+root.add_connection(a)
+root.add_connection(b)
+
+dup = copy.deepcopy(root)
+
