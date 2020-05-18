667fec83242d3c8e9ca828fae7c0ea09e1cf6a33
Felix Pan <tinylambda@gmail.com>
2020-05-18 13:36:50 +0800

    Add contextlib all

---
+++ module_contextlib/contextlib_exitstack_callbacks.py
@@ -0,0 +1,12 @@
+import contextlib
+
+
+def callback(*args, **kwargs):
+    print('closing callback({}, {})'.format(args, kwargs))
+
+
+with contextlib.ExitStack() as stack:
+    stack.callback(callback, 'arg1', 'arg2')
+    stack.callback(callback, arg3='arg3')
+
+
---
+++ module_contextlib/contextlib_exitstack_callbacks_error.py
@@ -0,0 +1,16 @@
+import contextlib
+
+
+def callback(*args, **kwargs):
+    print('closing callback({}, {})'.format(args, kwargs))
+
+
+try:
+    with contextlib.ExitStack() as stack:
+        stack.callback(callback, 'arg1', 'arg2')
+        stack.callback(callback, arg3='arg3')
+        raise RuntimeError('thrown error')
+except RuntimeError as err:
+    print('ERROR: {}'.format(err))
+
+
