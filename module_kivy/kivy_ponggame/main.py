ba8977a0e066a97f2378dfd14407f217adb0717a
Felix Pan <tinylambda@gmail.com>
2020-05-20 18:16:37 +0800

    The ball can move now

--- module_kivy/kivy_ponggame/main.py
+++ module_kivy/kivy_ponggame/main.py
@@ -1,7 +1,10 @@
 from kivy.app import App
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.clock import Clock
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
 from kivy.uix.widget import Widget
+
+from random import randint
 
 
 class PongBall(Widget):
@@ -20,12 +23,31 @@ class PongBall(Widget):
 
 
 class PongGame(Widget):
-    pass
+    ball = ObjectProperty(None)
+
+    def serve_ball(self):
+        self.ball.center = self.center
+        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+
+    def update(self, dt):
+        # call ball.move and other stuff
+        self.ball.move()
+
+        # bounce off top and bottom
+        if self.ball.y < 0 or self.ball.top > self.height:
+            self.ball.velocity_y *= -1
+
+        # bounce off left and right
+        if self.ball.x < 0 or self.ball.right > self.width:
+            self.ball.velocity_x *= -1
 
 
 class PongApp(App):
     def build(self):
-        return PongGame()
+        game = PongGame()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        return game
 
 
 if __name__ == '__main__':
