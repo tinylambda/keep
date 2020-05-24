90d4491278fa98ee4c321c8754c92c80c22802ae
Felix Pan <tinylambda@gmail.com>
2020-05-24 21:44:43 +0800

    More on kivy

--- module_kivy/kivy_paint/paint.py
+++ module_kivy/kivy_paint/paint.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.graphics import Color, Ellipse
+from kivy.graphics import Color, Ellipse, Line
 
 
 class MyPaintWidget(Widget):
@@ -9,6 +9,10 @@ class MyPaintWidget(Widget):
             Color(1, 1, 0)
             d = 30.
             Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
 
 
 class MyPaintApp(App):
