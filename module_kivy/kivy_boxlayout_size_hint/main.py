745ea0104d937afadf2ad63ee58609c4a82571c8
Felix Pan <tinylambda@gmail.com>
2020-06-20 18:52:09 +0800

    Add more kivy

---
+++ module_kivy/kivy_boxlayout_size_hint/main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+
+
+class ExampleApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    ExampleApp().run()
+
+
---
+++ module_kivy/kivy_hint_and_size/main.py
@@ -0,0 +1,45 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
+from kivy.properties import BooleanProperty, ObjectProperty
+
+
+class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior, RecycleBoxLayout):
+    """Add selection and focus behavior to the view"""
+    pass
+
+
+class SelectableLabel(RecycleDataViewBehavior, Label):
+    """Add selection support to the Label"""
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, rv, index, data):
+        """Catch and handle the view changes."""
+        self.index = index
+        return super(SelectableLabel, self).refresh_view_attrs(rv, index, data)
+
+    def on_touch_down(self, touch):
+        """Add selection on touch down"""
+        if super(SelectableLabel, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, rv, index, is_selected):
+        """Respond to the selection of items in the view."""
+        self.selected = is_selected
+
+
+class WeatherApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    WeatherApp().run()
+
