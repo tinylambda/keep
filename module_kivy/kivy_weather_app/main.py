1a91fa98ce55af6f52fd00424e7d98465a9b0a9e
Felix Pan <tinylambda@gmail.com>
2020-06-21 20:27:34 +0800

    Capture user input

--- module_kivy/kivy_weather_app/main.py
+++ module_kivy/kivy_weather_app/main.py
@@ -1,10 +1,13 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
 
 
 class AddLocationForm(BoxLayout):
+    search_input = ObjectProperty()
+
     def search_location(self):
-        print('Explicit is better than implicit.')
+        print('The user searched for "{}"'.format(self.search_input.text))
 
 
 class WeatherApp(App):
