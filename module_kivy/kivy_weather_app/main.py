37b6624887f50392a5f81b623d0dfb3b18f636e9
Felix Pan <tinylambda@gmail.com>
2020-06-22 22:40:41 +0800

    Show cities on UI

--- module_kivy/kivy_weather_app/main.py
+++ module_kivy/kivy_weather_app/main.py
@@ -1,11 +1,17 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
+from kivy.uix.label import Label
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.properties import ObjectProperty, BooleanProperty
 from kivy.network.urlrequest import UrlRequest
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
 
 
 class AddLocationForm(BoxLayout):
     search_input = ObjectProperty()
+    search_results = ObjectProperty()
 
     def search_location(self):
         print('The user searched for "{}"'.format(self.search_input.text))
@@ -17,8 +23,38 @@ class AddLocationForm(BoxLayout):
 
     def found_location(self, request, data):
         cities = ['{} ({})'.format(d['name'], d['sys']['country']) for d in data['list']]
-        print('\n'.join(cities))
+        self.search_results.data = [{'text': str(x)} for x in cities]
+        print(f"self.search_results.data={self.search_results.data}")
 
+
+class SelectableLabel(RecycleDataViewBehavior, Label):
+    """ Add selection support to the Label """
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, rv, index, data):
+        """ Catch and handle the view changes """
+        self.index = index
+        return super(SelectableLabel, self).refresh_view_attrs(
+            rv, index, data)
+
+    def on_touch_down(self, touch):
+        """ Add selection on touch down """
+        if super(SelectableLabel, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, rv, index, is_selected):
+        """ Respond to the selection of items in the view. """
+        self.selected = is_selected
+
+
+class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
+                                 RecycleBoxLayout):
+    """ Adds selection and focus behaviour to the view. """
+    
 
 class WeatherApp(App):
     pass
