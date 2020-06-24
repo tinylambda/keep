89a3df646544906da2f1646ec9c961fe891ce6e8
Felix Pan <tinylambda@gmail.com>
2020-06-24 19:30:16 +0800

    Add kivy recycleview selection behavior

--- module_kivy/kivy_recycleview/main.py
+++ module_kivy/kivy_recycleview/main.py
@@ -1,18 +1,44 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.recycleview import RecycleView
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.label import Label
+from kivy.properties import BooleanProperty
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 
 
-Builder.load_string('''
-<RV>:
-    viewclass: 'Label'
-    RecycleBoxLayout:
-        default_size: None, dp(56)
-        default_size_hint: 1, None
-        size_hint_y: None
-        height: self.minimum_height
-        orientation: 'vertical'
-''')
+class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior, RecycleBoxLayout):
+    """Adds selection and focus behavior to the view."""
+    pass
+
+
+class SelectableLabel(RecycleDataViewBehavior, Label):
+    """Add selection support to the Label."""
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, rv, index, data):
+        """Catch and handle the view changes"""
+        self.index = index
+        return super(SelectableLabel, self).refresh_view_attrs(rv, index, data)
+
+    def on_touch_down(self, touch):
+        """Add selection on touch down"""
+        if super(SelectableLabel, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, rv, index, is_selected):
+        """Respond to the selection of items in the view."""
+        self.selected = is_selected
+        if is_selected:
+            print('selection changed to {0}'.format(rv.data[index]))
+        else:
+            print('selection removed for {0}'.format(rv.data[index]))
 
 
 class RV(RecycleView):
