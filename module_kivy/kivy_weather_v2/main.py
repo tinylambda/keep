381f85c77a335919da1d9fc5cac41bcffda829a2
Felix Pan <tinylambda@gmail.com>
2020-07-06 15:03:07 +0800

    GPS enabled

--- module_kivy/kivy_weather_v2/main.py
+++ module_kivy/kivy_weather_v2/main.py
@@ -4,14 +4,18 @@ from kivy.app import App
 from kivy.network.urlrequest import UrlRequest
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.modalview import ModalView
+from kivy.uix.popup import Popup
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 from kivy.uix.recycleboxlayout import RecycleBoxLayout
 from kivy.uix.behaviors import FocusBehavior
 from kivy.properties import ObjectProperty, StringProperty, BooleanProperty, ListProperty, NumericProperty
 from kivy.storage.jsonstore import JsonStore
-from kivy.clock import Clock
+from kivy.clock import Clock, mainthread
+
+from plyer import gps
 
 WEATHER_APP_ID = '54f64871388416c8ff4a4b198787e8b1'
 
@@ -38,6 +42,9 @@ class WeatherRoot(BoxLayout):
     def show_add_location_form_widget(self):
         self.add_location_form_widget = AddLocationForm()
         self.add_location_form_widget.open()
+
+    def clear_dialog_widgets(self):
+        self.add_location_form_widget.dismiss()
 
     def show_current_weather(self, location=None):
         exists_origin_locations = [item['origin'] for item in self.locations_widget.locations_list.data]
@@ -73,19 +80,22 @@ class CurrentWeather(BoxLayout):
 
     def __init__(self, **kwargs):
         super(CurrentWeather, self).__init__(**kwargs)
+        self._weather_retrieved = self.weather_retrieved
 
     def update_weather(self):
         config = WeatherApp.get_running_app().config
         temp_type = config.getdefault('General', 'temp_type', 'metric').lower()
-        weather_template = 'http://api.openweathermap.org/data/2.5/weather?q={},{}&units={}&appid={}'
+        weather_template = 'https://api.openweathermap.org/data/2.5/weather?q={},{}&units={}&appid={}'
         weather_url = weather_template.format(
             urllib.parse.quote(self.location[0]),
             urllib.parse.quote(self.location[1]),
             temp_type, WEATHER_APP_ID
         )
+        print('Weather request: ', weather_url)
         request = UrlRequest(weather_url, self.weather_retrieved)
 
     def weather_retrieved(self, request, data):
+        print('Weather data: ', data)
         self.conditions = data['weather'][0]['description']
         self.temp = data['main']['temp']
         self.temp_min = data['main']['temp_min']
@@ -106,18 +116,27 @@ class AddLocationForm(ModalView):
     search_input = ObjectProperty()
     search_results = ObjectProperty()
 
+    def __init__(self, **kwargs):
+        self._found_location = self.found_location
+        super(AddLocationForm, self).__init__(**kwargs)
+
     def search_location(self):
-        search_template = 'http://api.openweathermap.org/data/2.5/find?q={}&type=like&appid={}'
+        search_template = 'https://api.openweathermap.org/data/2.5/find?q={}&type=like&appid={}'
+        query_location = self.search_input.text
         search_url = search_template.format(
-            urllib.parse.quote(self.search_input.text),
+            urllib.parse.quote(query_location),
             WEATHER_APP_ID
         )
-        request = UrlRequest(search_url, self.found_location)
+        print('Searching', search_url)
+        request = UrlRequest(search_url, self._found_location)
 
     def found_location(self, request, data):
         cities = [{'origin': (d['name'], d['sys']['country'])} for d in data['list']]
         self.search_results.data.clear()
         self.search_results.data.extend(cities)
+
+    def current_location(self):
+        print('Current Location....')
 
 
 class SelectableLabel(RecycleDataViewBehavior, Button):
@@ -158,7 +177,36 @@ class WeatherApp(App):
         return weather_root
 
     def on_pause(self):
+        gps.stop()
         print('On Pasuse....................................')
+
+    def on_stop(self):
+        gps.stop()
+        print('On stop ...................................')
+
+    def on_resume(self):
+        gps.start()
+        print('On resume....................................')
+
+    def on_start(self):
+        try:
+            gps.configure(on_location=self.on_location, on_status=self.on_status)
+            print('Now execute start....')
+            gps.start(minTime=1000, minDistance=1)
+        except NotImplementedError:
+            popup = Popup(titile='GPS Error',
+                          content=Label(text='GPS support is not implemented on your platform'))
+            popup.open()
+            Clock.schedule_once(lambda d: popup.dismiss(), 3)
+        print('On Start........................................')
+
+    @mainthread
+    def on_location(self, **kwargs):
+        print('Location: ', kwargs)
+
+    @mainthread
+    def on_status(self, **kwargs):
+        print('On Status: ', kwargs)
 
 
 if __name__ == '__main__':
