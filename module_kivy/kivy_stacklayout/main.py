991169d3c0d306b40c3350eb4d24e7ad73121407
Felix Pan <tinylambda@gmail.com>
2020-07-10 19:33:57 +0800

    Add StackLayout

---
+++ module_kivy/kivy_stacklayout/main.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.stacklayout import StackLayout
+
+
+class MyW(FloatLayout):
+    pass
+
+
+class MyApp(App):
+    def build(self):
+        return MyW()
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
