460922dbbae1e46ef9f14a2508d4fc8cce8f68b0
Felix Pan <tinylambda@gmail.com>
2020-07-10 17:47:07 +0800

    Add float layout

---
+++ module_kivy/kivy_floatlayout/main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
+
+
+class MyW(FloatLayout):
+    pass
+
+
+class MyApp(App):
+    def build(self):
+        app_root = MyW()
+        return app_root
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
