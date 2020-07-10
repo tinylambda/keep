70711cc763a5e928600fed30a86b5269210fc964
Felix Pan <tinylambda@gmail.com>
2020-07-10 19:51:12 +0800

    Add relative layout

---
+++ module_kivy/kivy_relativelayout/main.py
@@ -0,0 +1,29 @@
+from kivy.app import App
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.floatlayout import FloatLayout
+
+
+class MyW1(RelativeLayout):
+    def __init__(self, **kwargs):
+        super(MyW1, self).__init__(**kwargs)
+        self.size = (300, 300)
+        self.pos = (300, 0)
+
+
+class MyW(FloatLayout):
+    def __init__(self, **kwargs):
+        super(MyW, self).__init__(**kwargs)
+        self.myw1_name = MyW1()
+        self.add_widget(self.myw1_name)
+
+
+class MyApp(App):
+    def build(self):
+        app_root = MyW()
+        print(app_root.size)
+        return app_root
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
