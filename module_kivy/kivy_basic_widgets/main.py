092891278ff132fac71e17a89ebeeeaca7a50dc8
Felix Pan <tinylambda@gmail.com>
2020-07-17 13:23:22 +0800

    Kivy basic widgets

---
+++ module_kivy/kivy_basic_widgets/main.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput
+
+
+class MyW(Widget):
+    def my_callback(self):
+        self.ids.label1.text = 'Click!'
+
+    def my_callback1(self, inpt):
+        self.ids.label1.text = 'Enter !' + str(inpt)
+
+
+class MyApp(App):
+    def build(self):
+        return MyW()
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
+
