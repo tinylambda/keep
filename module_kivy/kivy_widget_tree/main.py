d7769d68b83359e2dcf6ecfc7acd036303da78e3
Felix Pan <tinylambda@gmail.com>
2020-07-18 23:14:22 +0800

    Kivy operate widgets

---
+++ module_kivy/kivy_widget_tree/main.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+
+
+class MyW(Widget):
+    def on_touch_down(self, touch):
+        self.add_widget(Button(text=str(touch.pos)))
+        if touch.is_double_tap:
+            self.remove_widget(self.ids.label1)
+        if touch.is_triple_tap:
+            self.clear_widgets()
+
+
+class MyApp(App):
+    def build(self):
+        return MyW()
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
