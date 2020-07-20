7aa88abf72e1f744d48f325d5614a44a6d473057
Felix Pan <tinylambda@gmail.com>
2020-07-20 09:06:30 +0800

    Kivy traverse widgets tree

---
+++ module_kivy/kivy_widgets_traverse/main.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+
+
+class MyW(Widget):
+    def on_touch_down(self, touch):
+        self.add_widget(Button(text=str(touch.pos), pos=touch.pos))
+        if touch.is_double_tap:
+            for child in self.children:
+                print(child)
+        if touch.is_triple_tap:
+            self.clear_widgets()
+
+
+class MyApp(App):
+    def build(self):
+        return MyW()
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
