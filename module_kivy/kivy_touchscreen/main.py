cf4d89c129c6b5e1bac70ca3072424fc1c351386
Felix Pan <tinylambda@gmail.com>
2020-07-22 01:16:25 +0800

    Kivy touch screen

---
+++ module_kivy/kivy_touchscreen/main.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MyW(Widget):
+    def on_touch_down(self, touch):
+        if 'button' in touch.profile:
+            self.ids.button1.text = touch.button
+
+        if 'pressure' in touch.profile:
+            self.ids.label1.text = str(touch.pressure)
+
+
+class MyApp(App):
+    def build(self):
+        return MyW()
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
