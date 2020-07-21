3261e1d5013b6513179ecf39c7bae0892d38e6c8
Felix Pan <tinylambda@gmail.com>
2020-07-21 16:00:51 +0800

    Kivy mouse touch down event

---
+++ module_kivy/kivy_mouse/main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MyW(Widget):
+    def on_touch_down(self, touch):
+        if 'button' in touch.profile:
+            self.ids.button1.text = touch.button
+
+
+class MyApp(App):
+    def build(self):
+        return MyW()
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
