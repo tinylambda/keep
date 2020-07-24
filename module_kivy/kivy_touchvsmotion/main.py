e59b9a815931f9fd8cbeac497f0228be6c9fd152
Felix Pan <tinylambda@gmail.com>
2020-07-24 17:27:04 +0800

    Kivy touch vs motion

---
+++ module_kivy/kivy_touchvsmotion/main.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+# We also know that a touch event always has the pos profile
+# , namely position information. The motion event, however,
+# is not dispatched throughout the widget tree.
+class MyW(Widget):
+    def on_touch_move(self, touch):
+        if 'pos' in touch.profile:
+            self.ids.button1.pos = touch.pos
+
+
+class MyApp(App):
+    def build(self):
+        return MyW()
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
