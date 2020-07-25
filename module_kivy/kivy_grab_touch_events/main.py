756d6b936f92b529513afa3b3374ad264395fcc2
Felix Pan <tinylambda@gmail.com>
2020-07-26 00:11:08 +0800

    Kivy grab touch events

---
+++ module_kivy/kivy_grab_touch_events/main.py
@@ -0,0 +1,27 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MyW(Widget):
+    def on_touch_down(self, touch):
+        if self.ids.button1.collide_point(*touch.pos):
+            touch.grab(self)
+            return True
+
+    def on_touch_up(self, touch):
+        if touch.grab_current is self:
+            self.ids.label1.text = 'Click on the screen but over the Button'
+            touch.ungrab(self)
+            return True
+        else:
+            self.ids.label1.text = 'Click on the screen'
+
+
+class MyApp(App):
+    def build(self):
+        return MyW()
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
