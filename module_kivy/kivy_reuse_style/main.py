16abd256b1d3b32d85d2689b839368fb18a6804a
Felix Pan <tinylambda@gmail.com>
2020-07-28 01:34:53 +0800

    Kivy reuse style

---
+++ module_kivy/kivy_reuse_style/main.py
@@ -0,0 +1,33 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
+from kivy.uix.boxlayout import BoxLayout
+
+
+class MyWidget1(BoxLayout):
+    def text(self, val):
+        print('text input text is : {txt}'.format(txt=val))
+
+
+class MyWidget2(BoxLayout):
+    writing = StringProperty()
+
+    def text(self, val):
+        self.writing = val
+
+
+class MyW(BoxLayout):
+    def __init__(self, **kwargs):
+        super(MyW, self).__init__(**kwargs)
+        self.add_widget(MyWidget1())
+        self.add_widget(MyWidget2())
+
+
+class MyApp(App):
+    def build(self):
+        return MyW()
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
