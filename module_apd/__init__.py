f5cc8f2df4a510998aa1baa3ca9a24d885b621da
Felix Pan <tinylambda@gmail.com>
2020-07-30 00:37:19 +0800

    get server status

---
+++ module_apd/__init__.py
@@ -0,0 +1,2 @@
+# Code from <<Advanced Python Development>>
+
---
+++ module_apd/apd_serverstatus/serverstatus.py
@@ -0,0 +1,33 @@
+import sys
+import socket
+import psutil
+
+
+def python_version():
+    return sys.version_info
+
+
+def ip_address():
+    hostname = socket.gethostname()
+    addresses = socket.getaddrinfo(hostname, None)
+    address_info = []
+    for address in addresses:
+        address_info.append((address[0].name, address[4][0]))
+    return address_info
+
+
+def cpu_load():
+    return psutil.cpu_percent()
+
+
+def ram_available():
+    return psutil.virtual_memory().available
+
+
+def ac_connected():
+    return psutil.sensors_battery().power_plugged
+
+
+if __name__ == '__main__':
+    print(ip_address())
+
