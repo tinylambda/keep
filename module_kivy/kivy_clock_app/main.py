1a4409ecb77d92814039442948356aa913586ac9
Felix Pan <tinylambda@gmail.com>
2020-07-31 01:55:54 +0800

    Kivy schedule function call

--- module_kivy/kivy_clock_app/main.py
+++ module_kivy/kivy_clock_app/main.py
@@ -1,21 +1,26 @@
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.core.text import LabelBase
 from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
 
-
-LabelBase.register(
-    name='Roboto',
-    fn_regular='fonts/roboto/Roboto-Thin.ttf',
-    fn_bold='fonts/roboto/Roboto-Medium.ttf',
-)
+from time import strftime
 
 
 class ClockApp(App):
-    pass
+    def update_time(self, nap):
+        self.root.ids.time.text = strftime('[b]%H[/b]:%M:%S')
+
+    def on_start(self):
+        Clock.schedule_interval(self.update_time, 1)
 
 
 if __name__ == '__main__':
+    LabelBase.register(
+        name='Roboto',
+        fn_regular='fonts/roboto/Roboto-Thin.ttf',
+        fn_bold='fonts/roboto/Roboto-Medium.ttf',
+    )
     Window.clearcolor = get_color_from_hex('#FF0080')
     ClockApp().run()
 
