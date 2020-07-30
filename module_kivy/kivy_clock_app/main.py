a6b0fca3348d45baa1aec1d5f00c7acacf90ed3e
Felix Pan <tinylambda@gmail.com>
2020-07-31 02:21:31 +0800

    Kivy use property to bind widget

--- module_kivy/kivy_clock_app/main.py
+++ module_kivy/kivy_clock_app/main.py
@@ -3,16 +3,25 @@ from kivy.clock import Clock
 from kivy.core.text import LabelBase
 from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
 
 from time import strftime
 
 
+class ClockLayout(BoxLayout):
+    time_prop = ObjectProperty(None)
+
+
 class ClockApp(App):
     def update_time(self, nap):
-        self.root.ids.time.text = strftime('[b]%H[/b]:%M:%S')
+        self.root.time_prop.text = strftime('[b]%H[/b]:%M:%S')
 
     def on_start(self):
         Clock.schedule_interval(self.update_time, 1)
+
+    def build(self):
+        return ClockLayout()
 
 
 if __name__ == '__main__':
