87c4f3e65cba934868bb548588c4b966b1380258
Felix Pan <tinylambda@gmail.com>
2020-08-10 23:53:45 +0800

    stop watch works

--- module_kivy/kivy_clock_app/main.py
+++ module_kivy/kivy_clock_app/main.py
@@ -10,15 +10,32 @@ from time import strftime
 
 
 class ClockApp(App):
-    pass
-    # def update_time(self, nap):
-    #     self.root.time_prop.text = strftime('[b]%H[/b]:%M:%S')
-    #
-    # def on_start(self):
-    #     Clock.schedule_interval(self.update_time, 1)
+    sw_seconds = 0
+    sw_started = False
 
-    # def build(self):
-    #     return ClockLayout()
+    def update_time(self, nap):
+        self.root.ids.time.text = strftime('[b]%H[/b]:%M:%S')
+
+    def update_stop_watch(self, nap):
+        if self.sw_started:
+            self.sw_seconds += nap
+        minutes, seconds = divmod(self.sw_seconds, 60)
+        self.root.ids.stopwatch.text = '%02d:%02d.[size=40]%02d[/size]' % \
+                                       (int(minutes), int(seconds), int(seconds * 100 % 100))
+
+    def on_start(self):
+        Clock.schedule_interval(self.update_time, 0)
+        Clock.schedule_interval(self.update_stop_watch, 0.016)
+
+    def start_stop(self):
+        self.root.ids.stopwatch.text = 'Start' if self.sw_started else 'Stop'
+        self.sw_started = not self.sw_started
+
+    def reset(self):
+        if self.sw_started:
+            self.root.ids.stopwatch.text = 'Start'
+            self.sw_started = False
+        self.sw_seconds = 0
 
 
 if __name__ == '__main__':
