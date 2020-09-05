905106b8052382cb4670d83ab662a0e4e989f9dd
Felix Pan <tinylambda@gmail.com>
2020-09-05 22:31:00 +0800

    functional programming more on stats

---
+++ functional_program/functional_sequence_operate/functional_stats_function.py
@@ -0,0 +1,53 @@
+import math
+from collections import Sequence
+
+
+def s0(samples: Sequence) -> float:
+    return sum(1 for x in samples)
+
+
+def s1(samples: Sequence) -> float:
+    return sum(x for x in samples)
+
+
+def s2(samples: Sequence) -> float:
+    return sum(x ** 2 for x in samples)
+
+
+def mean(samples: Sequence) -> float:
+    return s1(samples) / s0(samples)
+
+
+def stdev(samples: Sequence) -> float:
+    N = s0(samples)
+    return math.sqrt(s2(samples) / N - (s1(samples) / N) ** 2)
+
+
+def z(x, mu_x: float, rou_x: float) -> float:
+    return (x - mu_x) / rou_x
+
+
+def corr(samples1: Sequence, samples2: Sequence) -> float:
+    m_1, s_1 = mean(samples1), stdev(samples1)
+    m_2, s_2 = mean(samples2), stdev(samples2)
+
+    z_1 = (z(x, m_1, s_1) for x in samples1)
+    z_2 = (z(x, m_2, s_2) for x in samples2)
+
+    r = (sum(zx1 * zx2 for zx1, zx2 in zip(z_1, z_2))) / len(samples1)
+    return r
+
+
+if __name__ == '__main__':
+    d = [2, 4, 4, 4, 5, 5, 7, 9]
+    print(
+        list(z(x, mean(d), stdev(d)) for x in d)
+    )
+
+    xi = [1.47, 1.50, 1.52, 1.55, 1.57, 1.60, 1.63, 1.65, 1.68, 1.70, 1.73, 1.75, 1.78, 1.80, 1.83]
+    yi = [52.21, 53.12, 54.48, 55.84, 57.20, 58.57, 59.93, 61.29, 63.11, 64.47, 66.28, 68.10, 69.92, 72.19, 74.46]
+    print(
+        len(xi), len(yi),
+        round(corr(xi, yi), 5)
+    )
+
--- functional_program/functional_use_set/functional_xml.py
+++ functional_program/functional_use_set/functional_xml.py
@@ -1,6 +1,7 @@
 import ssl
 import urllib.request
 import xml.etree.ElementTree as XML
+from math import radians, sin, cos, sqrt, asin
 from typing import Text, List, TextIO, Iterable, Tuple, TypeVar, Iterator
 
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -58,6 +59,30 @@ def float_from_pair(lat_lon_iter: Text_Iter) -> LL_Iter:
     )
 
 
+MI = 3959
+NM = 3440
+KM = 6371
+Point = Tuple[float, float]
+
+
+def haversine(p1: Point, p2: Point, R: float=NM) -> float:
+    lat_1, lon_1 = p1
+    lat_2, lon_2 = p2
+
+    delta_lat = radians(lat_2 - lat_1)
+    delta_lon = radians(lon_2 - lon_1)
+
+    lat_1 = radians(lat_1)
+    lat_2 = radians(lat_2)
+
+    a = sqrt(
+        sin(delta_lat / 2) ** 2 +
+        cos(lat_1) * cos(lat_2) * sin(delta_lon / 2) ** 2
+    )
+    c = 2 * asin(a)
+    return R * c
+
+
 url = 'https://raw.githubusercontent.com/googlearchive/kml-samples/gh-pages/kml_handbook/mvdoqq.kml'
 with urllib.request.urlopen(url) as source:
     v1 = tuple(row_iter_kml(source))
@@ -74,8 +99,6 @@ with urllib.request.urlopen(url) as source:
     for item in legs(flt):
         print(item)
 
-print(tuple(flt))
-
 
 with urllib.request.urlopen(url) as source:
     v3 = legs(float_from_pair(
@@ -84,4 +107,15 @@ with urllib.request.urlopen(url) as source:
         )))
 print(tuple(v3))
 
+
+with urllib.request.urlopen(url) as source:
+    trip = (
+        (start, end, round(haversine(start, end), 4))
+        for start, end in
+        legs(float_from_pair(lat_lon_kml(row_iter_kml(source))))
+    )
+
+
+for start, end, dist in trip:
+    print(start, end, dist)
 
