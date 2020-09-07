aa4418308397b6513224ede76e3622a90783d9fe
Felix Pan <tinylambda@gmail.com>
2020-09-07 19:42:28 +0800

    functional more on sequence operate

---
+++ functional_program/functional_features/functional_organize_use_slice.py
@@ -0,0 +1,11 @@
+flat = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, ]
+
+print(len(flat))
+
+a = zip(flat[0::2], flat[1::2])
+print(list(a))
+
+n = 3
+b = zip(*(flat[i::n] for i in range(n)))
+print(list(b))
+
---
+++ functional_program/functional_features/functional_reverse.py
@@ -0,0 +1,19 @@
+from typing import Iterator
+
+
+def digits(x: int, b: int) -> Iterator[int]:
+    if x == 0:
+        return
+    yield x % b
+    for d in digits(x // b, b):
+        yield d
+
+
+def to_base(x: int, b: int) -> Iterator[int]:
+    return reversed(tuple(digits(x, b)))
+
+
+if __name__ == '__main__':
+    a = to_base(8, 2)
+    print(list(a))
+
