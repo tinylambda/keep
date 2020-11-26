d869cc60dcba97f9b0fb3bc8158aefeb59985d57
Felix Pan <tinylambda@gmail.com>
2020-11-26 15:42:48 +0800

    Optimize test script

--- module_requests/requests_get_performance_test.py
+++ module_requests/requests_get_performance_test.py
@@ -2,12 +2,24 @@ import time
 import requests
 
 
+URL1 = 'http://127.0.0.1:10000'
+URL2 = 'http://localhost:8000/chat/cc/'
+
+
 if __name__ == '__main__':
     start = time.time()
+    ex_count = 0
+
     for i in range(10000):
-        response = requests.get('http://127.0.0.1:8000/')
-        if response.status_code != 200:
-            print(response.status_code)
+        try:
+            response = requests.get(URL2)
+            if response.status_code != 200:
+                print(response.status_code)
+        except Exception as e:
+            ex_count += 1
+            print(e)
+            print('-' * 64)
 
     print('Cost: ', time.time() - start)
+    print('Exception count: ', ex_count)
 
---
+++ module_requests/requests_get_performance_test_multithread.py
@@ -0,0 +1,31 @@
+import time
+import requests
+import concurrent.futures
+
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+
+
+URL1 = 'http://127.0.0.1:10000'
+URL2 = 'http://localhost:8000/chat/cc/'
+
+
+def worker(url):
+    try:
+        response = requests.get(url)
+        if response.status_code != 200:
+            print(response.status_code)
+    except Exception as e:
+        print(e)
+        print('-' * 64)
+
+
+if __name__ == '__main__':
+    start = time.time()
+
+    pool = ThreadPoolExecutor(max_workers=16)
+    results = list(pool.map(worker, [URL2 for _ in range(10000)]))
+    print(len(results))
+    print(results[:10])
+
+    print('Cost: ', time.time() - start)
+
