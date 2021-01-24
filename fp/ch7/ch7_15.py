1b2f59b0a4cb5c89deeb81bdd5f412343a519021
Felix Pan <tinylambda@gmail.com>
2021-01-25 00:09:30 +0800

    Add more code about decorator

---
+++ fp/ch7/ch7_15.py
@@ -0,0 +1,49 @@
+from functools import singledispatch
+from collections import abc
+import numbers
+import html
+
+
+@singledispatch
+def htmlize(obj):
+    content = html.escape(repr(obj))
+    return '<pre>{}</pre>'.format(content)
+
+
+@htmlize.register(str)
+def _(text):
+    content = html.escape(text).replace('\n', '<br>\n')
+    return '<p>{}</p>'.format(content)
+
+
+@htmlize.register(numbers.Integral)
+def _(n):
+    return '<pre>{0} (0x{0:x})</pre>'.format(n)
+
+
+@htmlize.register(tuple)
+@htmlize.register(abc.MutableSequence)
+def _(seq):
+    inner = '</li>\n<li>'.join(htmlize(item) for item in seq)
+    return '<ul>\n<li>' + inner + '</li></ul>'
+
+
+class A:
+    pass
+
+
+if __name__ == '__main__':
+    print(
+        htmlize([1, 2, 3])
+    )
+
+    print(
+        htmlize('hello')
+    )
+
+    a = A()
+
+    print(
+        htmlize(a)
+    )
+
---
+++ fp/ch7/ch7_16.py
@@ -0,0 +1,34 @@
+registry = set()
+
+
+def register(active=True):
+    def decorate(func):
+        print('running register(active=%s) -> decorate(%s)' % (active, func))
+        if active:
+            registry.add(func)
+        else:
+            registry.discard(func)
+        return func
+    return decorate
+
+
+@register(active=False)
+def f1():
+    print('running f1()')
+
+
+@register()
+def f2():
+    print('running f2()')
+
+
+def f3():
+    print('running f3()')
+
+
+if __name__ == '__main__':
+    print(registry)
+    f1()
+    f2()
+    f3()
+
