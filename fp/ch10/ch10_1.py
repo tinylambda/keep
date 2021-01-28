1fdd6dfdebfa112e9e759e562c27fc60bb532348
Felix Pan <tinylambda@gmail.com>
2021-01-29 00:46:36 +0800

    init ch10

---
+++ fp/ch10/ch10_1.py
@@ -0,0 +1,51 @@
+from array import array
+import reprlib
+import math
+
+
+class Vector:
+    typecode = 'd'
+
+    def __init__(self, components):
+        self._components = array(self.typecode, components)
+
+    def __iter__(self):
+        return iter(self._components)
+
+    def __repr__(self):
+        components = reprlib.repr(self._components)
+        components = components[components.find('['):-1]
+        return 'Vector({})'.format(components)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) + bytes(self._components))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __abs__(self):
+        return math.sqrt(sum(x * x for x in self))
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(memv)
+
+
+if __name__ == '__main__':
+    v = Vector([3.1, 4.2])
+    print(v)
+
+    v = Vector((3, 4, 5))
+    print(v)
+
+    v = Vector(range(1000))
+    print(v)
+
