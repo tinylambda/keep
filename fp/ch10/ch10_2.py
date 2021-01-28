56acd382cf0fc9716363f20bae2136bc8b9337aa
Felix Pan <tinylambda@gmail.com>
2021-01-29 01:34:00 +0800

    Add more code about object

---
+++ fp/ch10/ch10_2.py
@@ -0,0 +1,67 @@
+from array import array
+import reprlib
+import math
+
+
+class Vector:
+    typecode = 'd'
+
+    def __init__(self, components):
+        self._components = array(self.typecode, components)
+
+    def __iter__(self):
+        return iter(self._components)
+
+    def __repr__(self):
+        components = reprlib.repr(self._components)
+        components = components[components.find('['):-1]
+        return 'Vector({})'.format(components)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) + bytes(self._components))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __abs__(self):
+        return math.sqrt(sum(x * x for x in self))
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(memv)
+
+    def __len__(self):
+        return len(self._components)
+
+    def __getitem__(self, item):
+        return self._components[item]
+
+
+if __name__ == '__main__':
+    v = Vector([3.1, 4.2])
+    print(v)
+
+    v = Vector((3, 4, 5))
+    print(v)
+
+    v = Vector(range(100))
+    print(v)
+
+    print(
+        len(v)
+    )
+    print(
+        v[0],
+        v[1],
+        v[2],
+    )
+    print(v[3:5])
+
---
+++ fp/ch10/ch10_3.py
@@ -0,0 +1,35 @@
+class MySeq:
+    def __getitem__(self, item):
+        return item
+
+
+if __name__ == '__main__':
+    s = MySeq()
+    print(
+        s[1:4]
+    )
+
+    print(
+        s[1:4:2]
+    )
+
+    print(
+        s[1:4:2, 9]
+    )
+
+    print(
+        s[1:4:2, 7:9]
+    )
+
+    print(
+        dir(slice)
+    )
+
+    help(
+        slice.indices
+    )
+
+    print(
+        s[1, 2]
+    )
+
