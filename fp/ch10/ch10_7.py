abaf72af712433acf35fb5ff64ccae3573042f8b
Felix Pan <tinylambda@gmail.com>
2021-01-31 20:40:40 +0800

    Add code about asyncio

---
+++ fp/ch10/ch10_7.py
@@ -0,0 +1,99 @@
+from array import array
+import reprlib
+import math
+import numbers
+import functools
+import operator
+
+
+class Vector:
+    typecode = 'd'
+    short_names = 'xyzt'
+
+    def __init__(self, components):
+        self._components = array(self.typecode, components)
+
+    def __iter__(self):
+        return iter(self._components)
+
+    def __repr__(self):
+        components = reprlib.repr(self._components)
+        components = components[components.find('['):-1]
+        return 'Vector({})'.format(components)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) + bytes(self._components))
+
+    def __eq__(self, other):
+        # return tuple(self) == tuple(other)
+
+        # if len(self) != len(other):
+        #     return False
+        # for a, b in zip(self, other):
+        #     if a != b:
+        #         return False
+        # return True
+        return len(self) == len(other) and all(a == b for a, b in zip(self, other))
+
+    def __hash__(self):
+        # hashes = (hash(x) for x in self._components)
+        hashes = map(hash, self._components)
+        return functools.reduce(operator.xor, hashes, 0)
+
+    def __abs__(self):
+        return math.sqrt(sum(x * x for x in self))
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(memv)
+
+    def __len__(self):
+        return len(self._components)
+
+    def __getitem__(self, item):
+        cls = self.__class__
+        if isinstance(item, slice):
+            return cls(self._components[item])
+        elif isinstance(item, numbers.Integral):
+            return self._components[item]
+        else:
+            msg = '{cls.__name__} indices must be integers'
+            raise TypeError(msg.format(cls=cls))
+
+    def __getattr__(self, item):
+        cls = type(self)
+        if len(item) == 1:
+            pos = cls.short_names.find(item)
+            if 0 <= pos < len(self._components):
+                return self._components[pos]
+        msg = '{.__name__!r} object has no attribute {!r}'
+        raise AttributeError(msg.format(cls, item))
+
+    def __setattr__(self, key, value):
+        cls = type(self)
+        if len(key) == 1:
+            if key in cls.short_names:
+                error = 'readonly attribute {attr_name!r}'
+            elif key.islower():
+                error = 'cannot set attribute "a" to "z" in {cls_name!r}'
+            else:
+                error = ''
+            if error:
+                msg = error.format(cls_name=cls.__name__, attr_name=key)
+                raise AttributeError(msg)
+            super(Vector, self).__setattr__(key, value)
+
+
+if __name__ == '__main__':
+    v = Vector([3.1, 4.2])
+    print(v)
+    print(hash(v))
+
---
+++ fp/ch18/ch18_3.py
@@ -0,0 +1,32 @@
+import asyncio
+import aiohttp
+
+from fp.ch17.ch17_1 import BASE_URL, save_flag, show, main
+
+
+async def get_flag(cc):
+    url = f'{BASE_URL}/{cc}/{cc}.gif'
+    async with aiohttp.request('GET', url) as resp:
+        image = await resp.read()
+        return image
+
+
+async def download_one(cc):
+    image = await get_flag(cc)
+    show(cc)
+    save_flag(image, cc.lower() + '.gif')
+    return cc
+
+
+def download_many(cc_list):
+    loop = asyncio.get_event_loop()
+    to_do = [download_one(cc) for cc in cc_list]
+    wait_coro = asyncio.wait(to_do)
+    res, _ = loop.run_until_complete(wait_coro)
+    loop.close()
+    return len(res)
+
+
+if __name__ == '__main__':
+    main(download_many)
+
