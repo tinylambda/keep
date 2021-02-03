842b2456a1f3e3d70fc8554d3af4b27adca7f8c6
Felix Pan <tinylambda@gmail.com>
2021-02-04 00:55:58 +0800

    Add more code about object

---
+++ fp/ch11/ch11_5.py
@@ -0,0 +1,35 @@
+import abc
+
+
+class Tombola(abc.ABC):
+    @abc.abstractmethod
+    def load(self, iterable):
+        """Add elements"""
+
+    @abc.abstractmethod
+    def pick(self):
+        """delete element and return it randomly"""
+
+    def loaded(self):
+        return bool(self.inspect())
+
+    def inspect(self):
+        """return ordered elements exists"""
+        items = []
+        while True:
+            try:
+                items.append(self.pick())
+            except LookupError:
+                break
+        self.load(items)
+        return tuple(sorted(items))
+
+
+if __name__ == '__main__':
+    class Fake(Tombola):
+        def pick(self):
+            return 13
+
+    print(Fake)
+    f = Fake()  # Error
+
