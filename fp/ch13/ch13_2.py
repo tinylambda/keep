7437aa375f99a4a19a6e9023dfabc5605e98395d
Felix Pan <tinylambda@gmail.com>
2021-02-05 23:49:14 +0800

    add operator related code

---
+++ fp/ch13/ch13_2.py
@@ -0,0 +1,31 @@
+import decimal
+import collections
+
+
+if __name__ == '__main__':
+    ctx = decimal.getcontext()
+    ctx.prec = 40
+
+    one_third = decimal.Decimal('1') / decimal.Decimal('3')
+    print(one_third)
+    print(
+        one_third == +one_third
+    )
+
+    ctx.prec = 28
+
+    print(
+        one_third == +one_third
+    )
+    print(+one_third)
+
+    ct = collections.Counter('abracadabra')
+    print(
+        ct
+    )
+    ct['r'] = -3
+    ct['d'] = 0
+    print(
+        +ct
+    )
+
