7182fc1fb29d6fa94ea8d7f97d2400238f53b40f
Felix Pan <tinylambda@gmail.com>
2021-02-14 21:40:11 +0800

    test generator

---
+++ playground/test_generator_return.py
@@ -0,0 +1,17 @@
+def g():
+    r = 0
+    for i in range(10):
+        yield i
+        r += i
+    print('returning ', r)
+    return r
+
+
+if __name__ == '__main__':
+    gg = g()
+    for i in range(10):
+        next(gg)
+    try:
+        next(gg)
+    except StopIteration as e:
+        print(e.value)
---
+++ playground/test_yield_from_list.py
@@ -0,0 +1,10 @@
+x = [1, 2, 3]
+
+
+def g():
+    yield from x
+
+
+if __name__ == '__main__':
+    for item in g():
+        print(item)
