9674dbd72710657dbac012dfc0d4fb91221b64ed
Felix Pan <tinylambda@gmail.com>
2021-02-14 22:47:11 +0800

    complete future

---
+++ fp/ch17/ch17_3.py
@@ -0,0 +1,38 @@
+from concurrent import futures
+
+from fp.ch17.ch17_1 import save_flag, get_flag, show, main
+
+
+MAX_WORKERS = 20
+
+
+def download_one(cc):
+    image = get_flag(cc)
+    show(cc)
+    save_flag(image, cc.lower() + '.gif')
+    return cc
+
+
+def download_many(cc_list):
+    cc_list = cc_list[:5]
+    with futures.ThreadPoolExecutor(max_workers=3) as executor:
+        to_do = []
+        for cc in sorted(cc_list):
+            future = executor.submit(download_one, cc)
+            to_do.append(future)
+            msg = 'Scheduled for {}: {}'
+            print(msg.format(cc, future))
+
+        results = []
+        for future in futures.as_completed(to_do):
+            res = future.result()
+            msg = '{} result: {}'
+            print(msg.format(future, res))
+            results.append(res)
+
+    return len(results)
+
+
+if __name__ == '__main__':
+    main(download_many)
+
