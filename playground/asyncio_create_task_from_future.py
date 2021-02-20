5e0dc9e4823503f0a3a0fd33c14309f10ba83645
Felix Pan <tinylambda@gmail.com>
2021-02-21 00:32:06 +0800

    sync code

--- playground/asyncio_create_task_from_future.py
+++ playground/asyncio_create_task_from_future.py
@@ -7,7 +7,18 @@ async def coro():
     await task
 
 
+async def coro2():
+    q = asyncio.Queue()
+    task = loop.create_task(q.get())
+    await task
+
+
+async def coro3():
+    task = loop.create_task(asyncio.sleep(2))
+    await task
+
+
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(coro())
+    loop.run_until_complete(coro3())
 
