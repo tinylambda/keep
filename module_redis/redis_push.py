2dff6b71db29b0a8c9cfcf52be18750d77b99244
Felix Pan <tinylambda@gmail.com>
2021-02-28 21:09:11 +0800

    add redis lpush

---
+++ module_redis/redis_push.py
@@ -0,0 +1,10 @@
+from module_redis import redis_client
+
+
+value = redis_client.lpush('lkey', 'hello')
+print(value)
+value = redis_client.lpush(b'lkey', b'world')
+print(value)
+value = redis_client.rpush('lkey', '!')
+print(value)
+
