7327adc1be6343fb267ea2c879edacb761c8892a
Felix Pan <tinylambda@gmail.com>
2021-03-28 02:13:35 +0800

    add module http.server

---
+++ module_http_server/http_server_errors.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler
+
+
+class ErrorHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_error(404)
+
+
+if __name__ == '__main__':
+    from http.server import HTTPServer
+    server = HTTPServer(('localhost', 9090), ErrorHandler)
+    print('Starting server, use <Ctrl+C> to stop')
+    server.serve_forever()
+
