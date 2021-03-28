30cb088622d7ebc7e83562385417d5e414a7a40c
Felix Pan <tinylambda@gmail.com>
2021-03-29 00:25:23 +0800

    add module webbrowser

---
+++ module_webbrowser/webbrowser_get.py
@@ -0,0 +1,6 @@
+import webbrowser
+
+
+b = webbrowser.get(using='firefox')
+b.open('https://docs.python.org/3/library/webbrowser.html')
+
---
+++ module_webbrowser/webbrowser_open.py
@@ -0,0 +1,4 @@
+import webbrowser
+
+webbrowser.open('https://docs.python.org/3/library/webbrowser.html')
+
