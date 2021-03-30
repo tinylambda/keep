60a0f0a68c745e96e6e2b645516740fd4bae6538
Felix Pan <tinylambda@gmail.com>
2021-03-31 00:15:44 +0800

    add code about os.path

---
+++ pc/ch5/ch5_13_3.py
@@ -0,0 +1,20 @@
+import os
+import os.path
+import glob
+
+
+pyfiles = glob.glob('*.py')
+name_sz_date = [
+    (name, os.path.getsize(name), os.path.getmtime(name))
+    for name in pyfiles
+]
+
+for name, size, mtime in name_sz_date:
+    print(name, size, mtime)
+
+print('-' * 64)
+
+file_metadata = [(name, os.stat(name)) for name in pyfiles]
+for name, meta in file_metadata:
+    print(name, meta.st_size, meta.st_mtime)
+
