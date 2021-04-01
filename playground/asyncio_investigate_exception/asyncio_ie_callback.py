9a6f1b622675a124a1e1fe8d6cdf46d08c23df9d
Felix <tinylambda@gmail.com>
2021-04-01 18:01:47 +0800

    asyncio task show exception immediately

---
+++ playground/asyncio_investigate_exception/asyncio_ie_callback.py
@@ -0,0 +1,41 @@
+import asyncio
+import logging
+
+
+def _handle_task_result(task: asyncio.Task):
+    try:
+        task.result()
+    except asyncio.CancelledError:
+        pass
+    except Exception:
+        logging.exception('exception raised by task = %r', task)
+
+
+async def problem():
+    await asyncio.sleep(1)
+    logging.warning("going to raise an exception now!")
+    raise RuntimeError("something went wrong")
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s.%(msecs)03d %(filename)s:%(lineno)d %(levelname)s %(message)s',
+        level=logging.INFO,
+        datefmt='%H:%M:%S'
+    )
+
+    loop = asyncio.get_event_loop()
+    logging.info("creating the problem task")
+    task = loop.create_task(problem())
+    task.add_done_callback(_handle_task_result)
+    logging.info('created task = %r', task)
+    logging.info('running the loop')
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        logging.info('closing the loop')
+        loop.close()
+
+    logging.info('shutting down')
+
---
+++ playground/asyncio_investigate_exception/asyncio_ie_problem.py
@@ -0,0 +1,31 @@
+import asyncio
+import logging
+
+
+async def problem():
+    await asyncio.sleep(1)
+    logging.warning("going to raise an exception now!")
+    raise RuntimeError("something went wrong")
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s.%(msecs)03d %(filename)s:%(lineno)d %(levelname)s %(message)s',
+        level=logging.INFO,
+        datefmt='%H:%M:%S'
+    )
+
+    loop = asyncio.get_event_loop()
+    logging.info("creating the problem task")
+    task = loop.create_task(problem())
+    logging.info('created task = %r', task)
+    logging.info('running the loop')
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        logging.info('closing the loop')
+        loop.close()
+
+    logging.info('shutting down')
+
