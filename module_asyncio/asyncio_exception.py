40b08c82d03e57bb6e59b428eb105ecd0ad15909
Felix Pan <tinylambda@gmail.com>
2021-04-02 01:18:39 +0800

    add code about encoding

---
+++ module_asyncio/asyncio_exception.py
@@ -0,0 +1,33 @@
+import asyncio
+import random
+
+
+async def simple_task(name):
+    print('in task', name)
+    randint = random.randint(4, 8)
+    await asyncio.sleep(randint)
+    raise RuntimeError(f'Error in task {name}')
+
+
+async def main():
+    loop = asyncio.get_event_loop()
+    tasks = [loop.create_task(simple_task(f'task_{i}')) for i in range(5)]
+    await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+
+    for task in tasks:
+        if task.done():
+            print(task.result())
+
+        task.cancel()
+        try:
+            await task
+        except asyncio.CancelledError:
+            pass
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    # loop.close()
+
+
---
+++ pc/ch5/ch5_14_2.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+
+print(sys.getdefaultencoding())
+
+# Write a file using a unicode file
+with open('jalape\xf1o.txt', 'w') as f:
+    f.write('Spicy!')
+
+
+print(os.listdir('.'))
+print(os.listdir(b'.'))
+
+with open(b'jalapen\xcc\x83o.txt') as f:
+    print(f.read())
+
