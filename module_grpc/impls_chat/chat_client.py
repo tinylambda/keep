8eb8d3b28219d4edd88471f08d94f70ce96d36ab
Felix Pan <tinylambda@gmail.com>
2021-04-04 02:55:13 +0800

    add more code on a grpc chat server

---
+++ module_grpc/impls_chat/chat_client.py
@@ -0,0 +1,86 @@
+import asyncio
+import logging
+import sys
+
+import grpc
+
+import chat_pb2
+import chat_pb2_grpc
+
+
+class ChatClient:
+    def __init__(self, host='127.0.0.1', port=50051):
+        self.channel = None
+        self.stub = None
+        self.host = host
+        self.port = port
+        self.loop = asyncio.get_event_loop()
+        self.input_q = asyncio.Queue()
+
+    def create_task(self, coro, name=None):
+        def _handle_task_result(completed_task: asyncio.Task):
+            try:
+                task.result()
+            except asyncio.CancelledError:
+                pass
+            except Exception as e:
+                logging.exception('Exception raised by task = %r', completed_task)
+        task = self.loop.create_task(coro)
+        task.set_name(name)
+        task.add_done_callback(_handle_task_result)
+        return task
+
+    def get_stdin_input(self):
+        asyncio.ensure_future(self.input_q.put(sys.stdin.readline()))
+
+    async def start(self):
+        self.loop.add_reader(sys.stdin, self.get_stdin_input)
+        # The channel to listen for the message stream
+        self.channel = grpc.aio.insecure_channel(f'{self.host}:{self.port}')
+        self.stub = chat_pb2_grpc.ChatServiceStub(channel=self.channel)
+        task_creation_callables = [
+            lambda: self.create_task(self.input_q.get(), 'user_input'),
+        ]
+
+        tasks = [f() for f in task_creation_callables]
+        while True:
+            await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+            for i, task in enumerate(tasks):
+                task_name = task.get_name()
+                if task.done():
+                    if task_name == 'user_input':
+                        user_input = task.result().strip()
+                        logging.info(f'got user input -> {user_input}')
+                        channel = chat_pb2.Channel(name='chan1', senders_name='felix')
+                        message = chat_pb2.Message(sender='felix', message=user_input, channel=channel)
+
+                        messages = [message]
+                        ack = await self.stub.SendMessage(messages)
+                        logging.info(f'got server ack => {ack.status}')
+                    else:
+                        pass
+                    tasks[i] = task_creation_callables[i]()
+
+    async def stop(self):
+        print('stopping')
+        await self.channel.close()
+        print('stopped')
+
+
+async def serve():
+    chat_client = ChatClient()
+    await chat_client.start()
+    await chat_client.stop()
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(serve())
+    loop.close()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()
+
+
--- module_grpc/impls_chat/chat_server.py
+++ module_grpc/impls_chat/chat_server.py
@@ -1,12 +1,35 @@
+import asyncio
+import collections
+import logging
+import grpc
+
 import chat_pb2
 import chat_pb2_grpc
 
 
 class ChatServicer(chat_pb2_grpc.ChatServiceServicer):
-    def JoinChannel(self, request, context):
-        pass
+    def __init__(self, *args, **kwargs):
+        super(ChatServicer, self).__init__(*args, **kwargs)
+        self.channel_cache = collections.defaultdict(set)
 
-    def SendMessage(self, request_iterator, context):
-        pass
+    async def JoinChannel(self, request: chat_pb2.Channel, context: grpc.aio.ServicerContext):
+        print(request)
+        print(context)
 
+    async def SendMessage(self, request_iterator, context: grpc.aio.ServicerContext) -> chat_pb2.MessageAck:
+        return chat_pb2.MessageAck(status='OK')
+
+
+async def serve() -> None:
+    server = grpc.aio.server()
+    chat_pb2_grpc.add_ChatServiceServicer_to_server(ChatServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    await server.start()
+    await server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(serve())
 
