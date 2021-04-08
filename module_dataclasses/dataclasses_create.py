05f5522cf2ab342919a835b5ed208e927359e0a1
Felix Pan <tinylambda@gmail.com>
2021-04-09 00:38:39 +0800

    add a dataclass example

---
+++ module_dataclasses/dataclasses_create.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class InventoryItem:
+    name: str
+    unit_price: float
+    quantity_on_hand: int = 0
+
+    def total_cost(self) -> float:
+        return self.unit_price * self.quantity_on_hand
+
+
+if __name__ == '__main__':
+    item = InventoryItem('item1', 5.6, 10)
+    print(item.total_cost())
+
+    print(item)
+
