8d8a9efd46f85416caf001c27084acb618ed5ccd
Felix <tinylambda@gmail.com>
2021-04-12 23:19:30 +0800

    add code about dataclasses

---
+++ module_dataclasses/dataclasses_asdict.py
@@ -0,0 +1,18 @@
+import dataclasses
+from dataclasses import dataclass
+
+
+@dataclass
+class InventoryItem:
+    name: str
+    unit_price: float
+    quantity_on_hand: int = 0
+
+    def total_cost(self) -> float:
+        return self.unit_price * self.quantity_on_hand
+
+
+if __name__ == '__main__':
+    item = InventoryItem('item1', 5.6, 10)
+    print(dataclasses.asdict(item))  # can transform to json using json.dumps
+
