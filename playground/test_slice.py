333f538199fdf0c1c7c33071f20acbc6701cb82f
Felix Pan <tinylambda@gmail.com>
2021-04-14 23:11:52 +0800

    add test for slicing

---
+++ playground/test_slice.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    test_list = [1, 2, 3, 4, 5]
+    header, *remaining = test_list
+    print(header)
+    print(remaining)
+
+    a, b, c, d = remaining
+    print(a, b, c, d)
+
