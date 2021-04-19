87c9dd6865cd9ba5b4e4b5cab5bb56fabc47cac6
Felix Pan <tinylambda@gmail.com>
2021-04-20 00:28:16 +0800

    add more code about re

---
+++ pc/ch2/ch2_6_2.py
@@ -0,0 +1,27 @@
+import re
+
+
+if __name__ == '__main__':
+    text = 'UPPER PYTHON, lower python, Mixed Python'
+    result = re.findall('python', text, flags=re.IGNORECASE)
+    print(result)
+
+    result = re.sub('python', 'snake', text, flags=re.IGNORECASE)
+    print(result)
+
+    def matchcase(word):
+        def replace(m):
+            text = m.group()
+            if text.isupper():
+                return word.upper()
+            elif text.islower():
+                return word.lower()
+            elif text[0].isupper():
+                return word.capitalize()
+            else:
+                return word
+        return replace
+
+    result = re.sub('python', matchcase('snake'), text, flags=re.IGNORECASE)
+    print(result)
+
---
+++ pc/ch2/ch2_7_2.py
@@ -0,0 +1,20 @@
+import re
+
+
+if __name__ == '__main__':
+    str_pat = re.compile(r'\"(.*)\"')
+    text1 = 'Computer says "no."'
+    print(
+        str_pat.findall(text1)
+    )
+
+    text2 = 'Computer says "no." Phone says "yes."'
+    print(
+        str_pat.findall(text2)
+    )
+
+    str_pat = re.compile(r'\"(.*?)\"')
+    print(
+        str_pat.findall(text2)
+    )
+
