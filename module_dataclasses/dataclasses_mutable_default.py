38fd1a0a4a0d2ff368e4a12bf1f1b6cefba9de85
Felix <tinylambda@gmail.com>
2021-04-26 16:37:24 +0800

    add code about dataclass mutable default

---
+++ module_dataclasses/dataclasses_mutable_default.py
@@ -0,0 +1,38 @@
+import typing
+from dataclasses import dataclass
+from dataclasses import field
+
+
+class C:
+    x = []
+
+    def add(self, element):
+        self.x.append(element)
+
+
+@dataclass
+class D:
+    x: typing.List = field(default_factory=list)
+
+    def add(self, element):
+        self.x.append(element)
+
+
+if __name__ == '__main__':
+    o1 = C()
+    o2 = C()
+    o1.add(1)
+    o2.add(2)
+    assert o1.x == [1, 2]
+    assert o1.x is o2.x
+
+    d1 = D()
+    d2 = D()
+    d1.add(1)
+    d2.add(2)
+
+    print(d1.x)
+    print(d2.x)
+
+    assert d1.x is not d2.x
+
