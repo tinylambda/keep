5702d3f4383c4012d86efef6604d3bf94be9d813
Felix <tinylambda@gmail.com>
2021-04-27 12:09:36 +0800

    add code about dataclasses

---
+++ module_dataclasses/dataclasses_init_only.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from dataclasses import field
+from dataclasses import InitVar
+
+
+class DatabaseType:
+    def lookup(self, name):
+        return 100
+
+
+@dataclass
+class C:
+    i: int
+    j: int = None
+    database: InitVar[DatabaseType] = None
+
+    def __post_init__(self, database: DatabaseType):
+        if self.j is None and database is not None:
+            self.j = database.lookup('j')
+
+
+if __name__ == '__main__':
+    c = C(10, database=DatabaseType())
+    print(c)
+
