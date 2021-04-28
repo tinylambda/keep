9cfa9ad89c8d3cc92e62c80cbb05795d30064926
Felix Pan <tinylambda@gmail.com>
2021-04-28 23:54:40 +0800

    add a simple expression evaluator

---
+++ pc/ch2/ch2_19_2.py
@@ -0,0 +1,88 @@
+import re
+import collections
+
+
+NUM = r'(?P<NUM>\d+)'
+PLUS = r'(?P<PLUS>\+)'
+MINUS = r'(?P<MINUS>-)'
+TIMES = r'(?P<TIMES>\*)'
+DIVIDE = r'(?P<DIVIDE>/)'
+LPAREN = r'(?P<LPAREN>\()'
+RPAREN = r'(?P<RPAREN>\))'
+WS = r'(?P<WS>\s+)'
+
+master_pat = re.compile('|'.join([NUM, PLUS, MINUS, TIMES, DIVIDE, LPAREN, RPAREN, WS]))
+Token = collections.namedtuple('Token', ['type', 'value'])
+
+
+def generate_tokens(text):
+    scanner = master_pat.scanner(text)
+    for m in iter(scanner.match, None):
+        tok = Token(m.lastgroup, m.group())
+        if tok.type != 'WS':
+            yield tok
+
+
+class ExpressionEvaluator:
+    def parse(self, text):
+        self.tokens = generate_tokens(text)
+        self.tok = None
+        self.nexttok = None
+        self._advance()
+        return self.expr()
+
+    def _advance(self):
+        self.tok, self.nexttok = self.nexttok, next(self.tokens, None)
+
+    def _accept(self, toktype):
+        if self.nexttok and self.nexttok.type == toktype:
+            self._advance()
+            return True
+        else:
+            return False
+
+    def _expect(self, toktype):
+        if not self._accept(toktype):
+            raise SyntaxError('Expected ' + toktype)
+
+    def expr(self):
+        exprval = self.term()
+        while self._accept('PLUS') or self._accept('MINUS'):
+            op = self.tok.type
+            right = self.term()
+            if op == 'PLUS':
+                exprval += right
+            elif op == 'MINUS':
+                exprval -= right
+        return exprval
+
+    def term(self):
+        termval = self.factor()
+        while self._accept('TIMES') or self._accept('DIVIDE'):
+            op = self.tok.type
+            right = self.factor()
+            if op == 'TIMES':
+                termval *= right
+            elif op == 'DIVIDE':
+                termval /= right
+        return termval
+
+    def factor(self):
+        if self._accept('NUM'):
+            return int(self.tok.value)
+        elif self._accept('LPAREN'):
+            exprval = self.expr()
+            self._expect('RPAREN')
+            return exprval
+        else:
+            raise SyntaxError('expect NUMBER or LPAREN')
+
+
+if __name__ == '__main__':
+    e = ExpressionEvaluator()
+    print(e.parse('2'))
+    print(e.parse('2 + 3'))
+    print(e.parse('2 + 3 * 4'))
+    print(e.parse('2 + (3 + 4) * 5'))
+    # print(e.parse('2 + (3 + * 4)'))
+
