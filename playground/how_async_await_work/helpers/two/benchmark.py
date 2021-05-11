a8f1f26fe3877eb076c70dbedd8895e3d8b4eea5
Felix <tinylambda@gmail.com>
2021-05-11 09:56:03 +0800

    coroutine is faster

---
+++ playground/how_async_await_work/helpers/two/benchmark.py
@@ -0,0 +1,37 @@
+from coroutine import coroutine
+from timeit import timeit
+
+
+# An object
+class GrepHandler(object):
+    def __init__(self, pattern, target):
+        self.pattern = pattern
+        self.target = target
+
+    def send(self, line):
+        if self.pattern in line:
+            self.target.send(line)
+
+
+@coroutine
+def grep(pattern, target):
+    while True:
+        line = yield
+        if pattern in line:
+            target.send(line)
+
+
+@coroutine
+def null():
+    while True:
+        item = yield
+
+
+if __name__ == '__main__':
+    line = 'python is nice'
+    p1 = grep('python', null())
+    p2 = GrepHandler('python', null())
+
+    print('coroutine', timeit('p1.send(line)', 'from __main__ import line, p1'))
+    print('object', timeit('p2.send(line)', 'from __main__ import line, p2'))
+
