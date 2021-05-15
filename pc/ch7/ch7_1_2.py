a8b383e6837f41d61b1f2bbdcb2ce48446251f3e
Felix Pan <tinylambda@gmail.com>
2021-05-15 22:24:12 +0800

    function basic

---
+++ pc/ch7/ch7_1_2.py
@@ -0,0 +1,31 @@
+import html
+
+
+def avg(first, *rest):
+    return (first + sum(rest)) / (1 + len(rest))
+
+
+def make_element(name, value, **attrs):
+    keyvals = [' %s="%s"' % item for item in attrs.items()]
+    attr_str = ''.join(keyvals)
+    element = f'<{name}{attr_str}>{html.escape(value)}</{name}>'
+    return element
+
+
+def anyargs(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+
+if __name__ == '__main__':
+    print(avg(1, 2))
+    print(avg(1, 2, 3, 4))
+
+    print(
+        make_element('item', 'Albatross', size='large', quantity=6)
+    )
+
+    print(
+        make_element('p', '<spam>')
+    )
+
