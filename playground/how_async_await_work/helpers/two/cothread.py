eb9941642c8b821cb027e39de696eb86e0d3d3e4
Felix <tinylambda@gmail.com>
2021-05-16 20:17:33 +0800

    use coroutine with thread

---
+++ playground/how_async_await_work/helpers/two/cothread.py
@@ -0,0 +1,36 @@
+from coroutine import coroutine
+from queue import Queue
+from threading import Thread
+from cofollow import follow, printer
+
+
+@coroutine
+def threaded(target):
+    messages = Queue()
+
+    def run_target():
+        while True:
+            item = messages.get()
+            if item is GeneratorExit:
+                target.close()
+                return
+            else:
+                target.send(item)
+
+    Thread(target=run_target).start()
+    try:
+        while True:
+            item = yield
+            messages.put(item)
+    except GeneratorExit:
+        messages.put(GeneratorExit)
+
+
+if __name__ == '__main__':
+    coro = threaded(printer())
+
+    for i in range(10):
+        coro.send(i)
+
+    coro.close()  # raise GeneratorExit inside generator
+
