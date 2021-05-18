8e31f97dfbf8aaf48cc07d957e9055bcce6ae200
Felix <tinylambda@gmail.com>
2021-05-18 15:24:49 +0800

    add test for string join

---
+++ playground/how_async_await_work/helpers/two/coprocess.py
@@ -0,0 +1,33 @@
+import pickle
+import subprocess
+import sys
+
+from concurrent.futures import ProcessPoolExecutor
+from coroutine import coroutine
+from cofollow import follow, printer
+
+
+@coroutine
+def sendto(f):
+    try:
+        while True:
+            item = yield
+            pickle.dump(item, f)
+            f.flush()
+    except StopIteration:
+        f.close()
+
+
+def recvfrom(f, target):
+    try:
+        while True:
+            item = pickle.load(f)
+            target.send(item)
+    except EOFError:
+        target.close()
+
+
+if __name__ == '__main__':
+    pass
+
+
