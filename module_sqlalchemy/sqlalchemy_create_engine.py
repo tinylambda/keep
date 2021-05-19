13ec39282a3bafccb331c735a983191ac135fce1
Felix <tinylambda@gmail.com>
2021-05-19 11:49:44 +0800

    add tool to generate sha256 strings

--- module_sqlalchemy/sqlalchemy_create_engine.py
+++ module_sqlalchemy/sqlalchemy_create_engine.py
@@ -2,6 +2,10 @@ from sqlalchemy import create_engine
 
 
 # With `echo` enabled, we’ll see all the generated SQL produced
+# The sole purpose of the Engine object from a user-facing perspective is to provide a unit of connectivity to the
+# database called Connection. When working with the Core directly, the Connection object is how all interaction with
+# the database is done. As the context, and the best way to do that is using Python context manager form, also known as
+# the with statement.
 engine = create_engine('sqlite:///memory:', echo=True)
 print(engine)
 
---
+++ playground/sampler/sampler_generate_sha256_strings.py
@@ -0,0 +1,12 @@
+import sys
+import hashlib
+
+
+if __name__ == '__main__':
+    inputs = sys.argv[1:]
+    for item in inputs:
+        item = item.strip()
+        item_bytes = item.encode('utf-8')
+        result = hashlib.sha256(item_bytes).hexdigest()
+        print(result)
+
