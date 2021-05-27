889859ac602a0be524cdf4fe8c7b860ac0d396a9
Felix Pan <tinylambda@gmail.com>
2021-05-27 23:37:04 +0800

    complete ch7

---
+++ pc/ch7/ch7_12_2.py
@@ -0,0 +1,26 @@
+def sample():
+    n = 0
+
+    def func():
+        print('n=', n)
+
+    def get_n():
+        return n
+
+    def set_n(value):
+        nonlocal n
+        n = value
+
+    func.get_n = get_n
+    func.set_n = set_n
+
+    return func
+
+
+if __name__ == '__main__':
+    f = sample()
+    f()
+    f.set_n(10)
+    f()
+    print(f.get_n())
+
---
+++ pc/ch7/ch7_12_3.py
@@ -0,0 +1,63 @@
+import sys
+
+
+class ClosureInstance:
+    def __init__(self, locals=None):
+        if locals is None:
+            locals = sys._getframe(1).f_locals
+
+        self.__dict__.update((key, value) for key, value in locals.items() if callable(value))
+
+    def __len__(self):
+        return self.__dict__['__len__']()
+
+
+def Stack():
+    items = []
+
+    def push(item):
+        items.append(item)
+
+    def pop():
+        return items.pop()
+
+    def __len__():
+        return len(items)
+
+    return ClosureInstance()
+
+
+class Stack2:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def __len__(self):
+        return len(self.items)
+
+
+if __name__ == '__main__':
+    s = Stack()
+    print(s)
+    s.push(10)
+    s.push(20)
+    s.push('hello')
+    print(len(s))
+
+    print(s.pop())
+    print(s.pop())
+    print(s.pop())
+
+    from timeit import timeit
+    s = Stack()
+    r = timeit('s.push(1);s.pop()', 'from __main__ import s')
+    print(r)
+
+    s = Stack2()
+    r = timeit('s.push(1);s.pop()', 'from __main__ import s')
+    print(r)
