ccdf0cd188950751d8a10090beed89fa0821e494
Felix Pan <tinylambda@gmail.com>
2021-05-28 22:05:53 +0800

    how to print a instance

---
+++ pc/ch8/ch8_1_2.py
@@ -0,0 +1,20 @@
+class Pair:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Pair({0.x!r}, {0.y!r})'.format(self)
+
+    def __str__(self):
+        return '({0.x!s}, {0.y!s})'.format(self)
+
+
+if __name__ == '__main__':
+    p = Pair(3, 4)
+    print(repr(p))
+    print(p)
+
+    print('p is {0!r}'.format(p))
+    print('p is {0}'.format(p))
+
