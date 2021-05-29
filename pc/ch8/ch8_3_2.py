35ef8e09b13ef2bb796c62e004d959a21fa1beac
Felix Pan <tinylambda@gmail.com>
2021-05-29 23:09:30 +0800

    use context manager

---
+++ pc/ch8/ch8_3_2.py
@@ -0,0 +1,32 @@
+from socket import socket, AF_INET, SOCK_STREAM
+
+
+class LazyConnection:
+    def __init__(self, address, family=AF_INET, type=SOCK_STREAM):
+        self.address = address
+        self.family = family
+        self.type = type
+        self.sock = None
+
+    def __enter__(self):
+        if self.sock is not None:
+            raise RuntimeError('already connected')
+        self.sock = socket(self.family, self.type)
+        self.sock.connect(self.address)
+        return self.sock
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.sock.close()
+        self.sock = None
+
+
+if __name__ == '__main__':
+    from functools import partial
+    conn = LazyConnection(('www.python.org', 80))
+    with conn as s:
+        s.send(b'GET /index.html HTTP/1.0\r\n')
+        s.send(b'Host: www.python.org\r\n')
+        s.send(b'\r\n')
+        resp = b''.join(iter(partial(s.recv, 8192), b''))
+        print(resp)
+
