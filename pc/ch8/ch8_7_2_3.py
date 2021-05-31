1ea683fb9a424e156e306a56cb4db044af1cf2b8
Felix Pan <tinylambda@gmail.com>
2021-06-01 00:15:14 +0800

    add code about class

---
+++ pc/ch8/ch8_7_2_3.py
@@ -0,0 +1,14 @@
+class Proxy:
+    def __init__(self, obj):
+        self._obj = obj
+
+    def __getattr__(self, name):
+        return getattr(self._obj, name)
+
+    def __setattr__(self, name, value):
+        if name.startswith('_'):
+            super(Proxy, self).__setattr__(name, value)
+        else:
+            setattr(self._obj, name, value)
+
+    
---
+++ pc/ch8/ch8_7_3.py
@@ -0,0 +1,27 @@
+class Base:
+    def __init__(self):
+        print('Base.__init__')
+
+
+class A(Base):
+    def __init__(self):
+        Base.__init__(self)
+        print('A.__init__')
+
+
+class B(Base):
+    def __init__(self):
+        Base.__init__(self)
+        print('B.__init__')
+
+
+class C(A, B):
+    def __init__(self):
+        A.__init__(self)
+        B.__init__(self)
+        print('C.__init__')
+
+
+if __name__ == '__main__':
+    c = C()
+
