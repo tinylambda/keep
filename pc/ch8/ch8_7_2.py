c680ccd17c53a34bb9e0116849ab74d5cdb33c43
Felix Pan <tinylambda@gmail.com>
2021-06-01 00:15:14 +0800

    add code about class

---
+++ pc/ch8/ch8_7_2.py
@@ -0,0 +1,16 @@
+class A:
+    def spam(self):
+        print('A.spam')
+
+
+class B(A):
+    def spam(self):
+        print('B.spam')
+        super().spam()
+
+
+if __name__ == '__main__':
+    b = B()
+    b.spam()
+
+
