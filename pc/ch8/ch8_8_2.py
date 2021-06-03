4f3aabeb126ee853bdeb29ce17304c4138fca028
Felix Pan <tinylambda@gmail.com>
2021-06-03 23:56:51 +0800

    simple class for property

---
+++ pc/ch8/ch8_8_2.py
@@ -0,0 +1,19 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError('expected a string')
+        self._name = value
+
+    @name.deleter
+    def name(self):
+        raise AttributeError('cannot delete attribute')
+
+
