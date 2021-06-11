97cf64f7e4dabb5eec94b77dcc3155183ff8b1a2
Felix Pan <tinylambda@gmail.com>
2021-06-12 04:14:46 +0800

    simple way to init object

---
+++ pc/ch8/ch8_10_3.py
@@ -0,0 +1,37 @@
+import math
+
+
+def lazyproperty(func):
+    name = f'_lazy_{func.__name__}'
+
+    @property
+    def lazy(self):
+        if hasattr(self, name):
+            return getattr(self, name)
+        else:
+            value = func(self)
+            setattr(self, name, value)
+            return value
+    return lazy
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    @lazyproperty
+    def area(self):
+        print('Computing area')
+        return math.pi * self.radius ** 2
+
+    @lazyproperty
+    def perimeter(self):
+        print('computing perimeter')
+        return 2 * math.pi * self.radius
+
+
+if __name__ == '__main__':
+    c = Circle(4.0)
+    print(c.area)
+    c.area = 25
+
---
+++ pc/ch8/ch8_11_2_3.py
@@ -0,0 +1,26 @@
+class Structure:
+    _fields = []
+
+    def __init__(self, *args, **kwargs):
+        if len(args) != len(self._fields):
+            raise TypeError(f'expected {len(self._fields)} arguments')
+
+        for name, value in zip(self._fields, args):
+            setattr(self, name, value)
+
+        extra_args = kwargs.keys() - self._fields
+        for name in extra_args:
+            setattr(self, name, kwargs.pop(name))
+
+        if kwargs:
+            raise TypeError(f'duplicate values for {",".join(kwargs)}')
+
+
+if __name__ == '__main__':
+    class Stock(Structure):
+        _fields = ['name', 'shares', 'price']
+
+    s1 = Stock('ACME', 50, 91.1)
+    s2 = Stock('ACME', 50, 91.1, date='8/2/2012')
+    s2 = Stock('ACME', 50, 91.1, date='8/2/2012', price=100)
+
