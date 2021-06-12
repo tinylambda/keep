d3212bfe2494bbf417877ebe6ee89950e1611d79
Felix Pan <tinylambda@gmail.com>
2021-06-12 21:13:07 +0800

    abstract class in python

---
+++ pc/ch8/ch8_12_3.py
@@ -0,0 +1,16 @@
+import collections.abc
+
+
+if __name__ == '__main__':
+    item = object()
+
+    print(isinstance(item, collections.abc.Sequence))
+    print(isinstance(item, collections.abc.Iterable))
+    print(isinstance(item, collections.abc.Sized))
+    print(isinstance(item, collections.abc.Mapping))
+
+    from decimal import Decimal
+    import numbers
+
+    x = Decimal('3.4')
+    print(isinstance(x, numbers.Real))
