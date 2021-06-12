b0ea7e5448f4cee5cb5c525827550dd44d8f570b
Felix Pan <tinylambda@gmail.com>
2021-06-12 21:13:07 +0800

    abstract class in python

---
+++ pc/ch8/ch8_12_2.py
@@ -0,0 +1,55 @@
+import io
+
+from abc import ABCMeta, abstractmethod
+
+
+class IStream(metaclass=ABCMeta):
+    @abstractmethod
+    def read(self, maxbytes=-1):
+        pass
+
+    @abstractmethod
+    def write(self, data):
+        pass
+
+
+class StockItem(IStream):
+    def read(self, maxbytes=-1):
+        pass
+
+    def write(self, data):
+        pass
+
+
+class A(metaclass=ABCMeta):
+    @property
+    @abstractmethod
+    def name(self):
+        pass
+
+    @name.setter
+    @abstractmethod
+    def name(self, value):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def method1(cls):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def method2():
+        pass
+
+
+if __name__ == '__main__':
+    # a = IStream()  # Can't instantiate abstract class IStream with abstract methods read, write
+
+    s = StockItem()
+
+    IStream.register(io.IOBase)
+    f = open('/tmp/test.txt', 'w')
+    print(isinstance(f, IStream))
+
+
