f22546606d4c3cb9260f69b4678ea98f9c6d1c60
Felix <tinylambda@gmail.com>
2021-06-16 11:17:52 +0800

    how to read and write a excel file

---
+++ module_openpyxl/openpyxl_simple_write.py
@@ -0,0 +1,12 @@
+from openpyxl import Workbook
+
+
+if __name__ == '__main__':
+    workbook = Workbook()
+    sheet = workbook.active
+
+    sheet['A1'] = 'hello'
+    sheet['B1'] = 'world!'
+
+    workbook.save(filename='/Users/felix/Downloads/hello_world.xlsx')
+
