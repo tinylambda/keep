b69a551f7a372066f7abcbd293e2eaf49cc11cf6
Felix <tinylambda@gmail.com>
2021-06-18 13:49:09 +0800

    covert dict to objects

--- module_openpyxl/openpyxl_simple_toobjects.py
+++ module_openpyxl/openpyxl_simple_toobjects.py
@@ -17,8 +17,33 @@ if __name__ == '__main__':
     for value in sheet.iter_rows(min_row=1, max_row=1, values_only=True):
         header = [item for item in value]
 
+    products = []
+    reviews = []
     # iterate over all rows and create product objects and review objects
     for row in sheet.iter_rows(min_row=2, values_only=True):
         row_dict = dict(zip(header, row))
-        print(row_dict)
+
+        product_args = []
+        for field_name in product_field_names:
+            value = next((item
+                          for item in map(row_dict.get, [field_name, '_'.join(['product', field_name])])
+                          if item is not None),
+                         None)
+            product_args.append(value)
+        product = Product(*product_args)
+        products.append(product)
+
+        review_args = []
+        for field_name in review_field_names:
+            if field_name == 'stars':
+                field_name = 'star_rating'
+
+            value = next((item
+                          for item in map(row_dict.get, [field_name, '_'.join(['review', field_name])])
+                          if item is not None), None)
+            review_args.append(value)
+        review = Review(*review_args)
+        reviews.append(review)
+    print(products)
+    print(reviews)
 
