476f6a706cbf436656062b2b9cec0c9d6c89e50a
Felix Pan <tinylambda@gmail.com>
2021-06-20 00:58:15 +0800

    logging basic usage

---
+++ module_logging/logging_variable.py
@@ -0,0 +1,6 @@
+import logging
+
+
+if __name__ == '__main__':
+    logging.warning('%s before %s', 'Look', 'leap!')
+
---
+++ module_logging/sample_app/myapp.py
@@ -0,0 +1,14 @@
+import logging
+import mylib
+
+
+def main():
+    logging.basicConfig(filename='/tmp/myapp.log', level=logging.INFO)
+    logging.info('Started')
+    mylib.do_something()
+    logging.info('Finished')
+
+
+if __name__ == '__main__':
+    main()
+
