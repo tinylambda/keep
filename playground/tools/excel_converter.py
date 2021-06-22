a95bdb4dc1e968d4fa1731584fb3766abfa13eb2
Felix <tinylambda@gmail.com>
2021-06-22 18:25:12 +0800

    add module pytest

--- playground/tools/excel_converter.py
+++ playground/tools/excel_converter.py
@@ -2,6 +2,9 @@ from openpyxl import load_workbook
 
 if __name__ == '__main__':
     workbook = load_workbook(filename='/Users/felix/Downloads/olympic.questions.xlsx')
-
-
+    print(workbook.sheetnames)
+    print(workbook.active)
+    sheet = workbook.active
+    for row in sheet.iter_rows(values_only=True):
+        print(row)
 
