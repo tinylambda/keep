f45ee7a2bb84c5c49ed958192f554eff057567c4
Felix Pan <tinylambda@gmail.com>
2021-06-25 00:31:35 +0800

    add code about mixin

---
+++ module_logging/__init__.py
@@ -0,0 +1,17 @@
+import sys
+import logging
+import logging.handlers
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler(stream=sys.stderr)
+console_handler.setLevel(level=logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+
+
