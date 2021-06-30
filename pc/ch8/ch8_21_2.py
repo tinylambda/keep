185298c0e039e8e7a51a89a0c4fc4dec971182af
Felix Pan <tinylambda@gmail.com>
2021-07-01 00:46:49 +0800

    more code about object

---
+++ pc/ch8/ch8_21_2.py
@@ -0,0 +1,47 @@
+class Node:
+    pass
+
+
+class UnaryOperator(Node):
+    def __init__(self, operand):
+        self.operand = operand
+
+
+class BinaryOperator(Node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+class Add(BinaryOperator):
+    pass
+
+
+class Sub(BinaryOperator):
+    pass
+
+
+class Mul(BinaryOperator):
+    pass
+
+
+class Div(BinaryOperator):
+    pass
+
+
+class Negate(UnaryOperator):
+    pass
+
+
+class Number(Node):
+    def __init__(self, value):
+        self.value = value
+
+
+if __name__ == '__main__':
+    t1 = Sub(Number(3), Number(4))
+    t2 = Mul(Number(2), t1)
+    t3 = Div(t2, Number(5))
+    t4 = Add(Number(1), t3)
+
+
