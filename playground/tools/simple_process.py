911365ba9f588bcd2d930acbe0db1676645b09af
Felix <tinylambda@gmail.com>
2021-06-30 09:42:03 +0800

    add a tool script

---
+++ playground/tools/simple_process.py
@@ -0,0 +1,10 @@
+import csv
+
+
+if __name__ == '__main__':
+    f = '/Users/felix/Result_7.csv'
+    with open(f) as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            print('_'.join(row[:3]))
+
