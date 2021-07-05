9ebf104a575048ac3170f411c45dcf0fb1f63e4a
Felix Pan <tinylambda@gmail.com>
2021-07-06 01:16:34 +0800

    code about evaluate expressions

---
+++ books/pc/ch8/ch8_22_2.py
@@ -0,0 +1,107 @@
+import types
+
+
+class Node:
+    pass
+
+
+class NodeVisitor:
+    def visit(self, node):
+        stack = [node]
+        last_result = None
+        while stack:
+            try:
+                last = stack[-1]
+                if isinstance(last, types.GeneratorType):
+                    stack.append(last.send(last_result))
+                    last_result = None
+                elif isinstance(last, Node):
+                    stack.append(self._visit(stack.pop()))
+                else:
+                    last_result = stack.pop()
+            except StopIteration:
+                stack.pop()
+        return last_result
+
+    def _visit(self, node):
+        methname = f'visit_{type(node).__name__}'
+        meth = getattr(self, methname, None)
+        if meth is None:
+            meth = self.generic_visit
+        return meth(node)
+
+    def generic_visit(self, node):
+        raise RuntimeError(f'No {"visit_" + type(node).__name__} method')
+
+
+class UnaryOperator(Node):
+    def __init__(self, operand):
+        self.operand = operand
+
+
+class BinaryOperator(Node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+class Add(BinaryOperator):
+    pass
+
+
+class Sub(BinaryOperator):
+    pass
+
+
+class Mul(BinaryOperator):
+    pass
+
+
+class Div(BinaryOperator):
+    pass
+
+
+class Negate(UnaryOperator):
+    pass
+
+
+class Number(Node):
+    def __init__(self, value):
+        self.value = value
+
+
+class Evaluator(NodeVisitor):
+    def visit_Number(self, node):
+        return node.value
+
+    def visit_Add(self, node):
+        return self.visit(node.left) + self.visit(node.right)
+
+    def visit_Sub(self, node):
+        return self.visit(node.left) - self.visit(node.right)
+
+    def visit_Mul(self, node):
+        return self.visit(node.left) * self.visit(node.right)
+
+    def visit_Div(self, node):
+        return self.visit(node.left) / self.visit(node.right)
+
+    def visit_Negate(self, node):
+        return -self.visit(node.operand)
+
+
+if __name__ == '__main__':
+    t1 = Sub(Number(3), Number(4))
+    t2 = Mul(Number(2), t1)
+    t3 = Div(t2, Number(5))
+    t4 = Add(Number(1), t3)
+
+    e = Evaluator()
+    print(e.visit(t4))
+
+    a = Number(0)
+    for n in range(100000):
+        a = Add(a, Number(n))
+
+    e.visit(a)
+
---
+++ books/pc/ch8/ch8_22_2_2.py
@@ -0,0 +1,30 @@
+from books.pc.ch8.ch8_22_2 import NodeVisitor, Number, Add
+
+
+class Evaluator(NodeVisitor):
+    def visit_Number(self, node):
+        return node.value
+
+    def visit_Add(self, node):
+        yield (yield node.left) + (yield node.right)
+
+    def visit_Sub(self, node):
+        yield (yield node.left) - (yield node.right)
+
+    def visit_Mul(self, node):
+        yield (yield node.left) * (yield node.right)
+
+    def visit_Div(self, node):
+        yield (yield node.left) / (yield node.right)
+
+    def visit_Negate(self, node):
+        yield -(yield node.operand)
+
+
+if __name__ == '__main__':
+    e = Evaluator()
+    a = Number(0)
+    for n in range(100000):
+        a = Add(a, Number(n))
+    print(e.visit(a))
+
