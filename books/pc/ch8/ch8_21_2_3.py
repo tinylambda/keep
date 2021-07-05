5b15315e49c261b0a3fa949f137b7485fa90f058
Felix Pan <tinylambda@gmail.com>
2021-07-06 01:16:34 +0800

    code about evaluate expressions

---
+++ books/pc/ch8/ch8_21_2_3.py
@@ -0,0 +1,48 @@
+from books.pc.ch8.ch8_21_2 import Number, Mul, Div, Add, Sub
+from books.pc.ch8.ch8_21_2_2 import NodeVisitor
+
+
+class StackCode(NodeVisitor):
+    def generate_code(self, node):
+        self.instructions = []
+        self.visit(node)
+        return self.instructions
+
+    def visit_Number(self, node):
+        self.instructions.append(('PUSH', node.value))
+
+    def binop(self, node, instruction):
+        self.visit(node.left)
+        self.visit(node.right)
+        self.instructions.append((instruction, ))
+
+    def visit_Add(self, node):
+        self.binop(node, 'ADD')
+
+    def visit_Sub(self, node):
+        self.binop(node, 'SUB')
+
+    def visit_Mul(self, node):
+        self.binop(node, 'MUL')
+
+    def visit_Div(self, node):
+        self.binop(node, 'DIV')
+
+    def unaryop(self, node, instruction):
+        self.visit(node.operand)
+        self.instructions.append((instruction, ))
+
+    def visit_Negate(self, node):
+        self.unaryop(node, 'NEG')
+
+
+if __name__ == '__main__':
+    t1 = Sub(Number(3), Number(4))
+    t2 = Mul(Number(2), t1)
+    t3 = Div(t2, Number(5))
+    t4 = Add(Number(1), t3)
+
+    s = StackCode()
+    r = s.generate_code(t4)
+    print(r)
+
