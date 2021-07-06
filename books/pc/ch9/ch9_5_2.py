17c3a2c652868ea4cc07418b1fc05a886e28e881
Felix <tinylambda@gmail.com>
2021-07-06 09:33:05 +0800

    add code about decorator

---
+++ books/pc/ch9/ch9_5_2.py
@@ -0,0 +1,60 @@
+from functools import wraps, partial
+import logging
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s:%(message)s',
+    level=logging.DEBUG,
+    datefmt='%m/%d/%Y %I:%M:%S %p'
+)
+
+
+def attach_wrapper(obj, func=None):
+    if func is None:
+        return partial(attach_wrapper, obj)
+    setattr(obj, func.__name__, func)
+    return func
+
+
+def logged(level, name=None, message=None):
+    def decorate(func):
+        logname = name if name else func.__module__
+        log = logging.getLogger(logname)
+        logmsg = message if message else func.__name__
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            log.log(level, logmsg)
+            return func(*args, **kwargs)
+
+        @attach_wrapper(wrapper)
+        def set_level(newlevel):
+            nonlocal level
+            level = newlevel
+
+        @attach_wrapper(wrapper)
+        def set_message(newmsg):
+            nonlocal logmsg
+            logmsg = newmsg
+        return wrapper
+    return decorate
+
+
+if __name__ == '__main__':
+    @logged(logging.DEBUG)
+    def add(x, y):
+        return x + y
+
+    @logged(logging.CRITICAL, 'example')
+    def spam():
+        print('spam')
+
+    add(1, 2)
+    add.set_message('Add called')
+    add(1, 2)
+    add.set_level(logging.WARNING)
+    add(1, 2)
+
+
+
+
---
+++ playground/simple/cache/simple_cache.py
@@ -0,0 +1,11 @@
+import asyncio
+
+
+class SimpleCache:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
