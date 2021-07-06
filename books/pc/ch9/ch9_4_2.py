addd43fcdad8fef829d89c098cd8072fd73f5022
Felix <tinylambda@gmail.com>
2021-07-06 09:33:05 +0800

    add code about decorator

---
+++ books/pc/ch9/ch9_4_2.py
@@ -0,0 +1,38 @@
+from functools import wraps
+import logging
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s:%(message)s',
+    level=logging.DEBUG,
+    datefmt='%m/%d/%Y %I:%M:%S %p'
+)
+
+
+def logged(level, name=None, message=None):
+    def decorate(func):
+        logname = name if name else func.__module__
+        log = logging.getLogger(logname)
+        logmsg = message if message else func.__name__
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            log.log(level, logmsg)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorate
+
+
+@logged(logging.DEBUG)
+def add(x, y):
+    return x + y
+
+
+@logged(logging.CRITICAL, 'example')
+def spam():
+    print('spam')
+
+
+if __name__ == '__main__':
+    add(1, 2)
+    spam()
