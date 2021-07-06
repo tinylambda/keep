797c3477c6dca726c9ce5321704465df11e423cb
Felix <tinylambda@gmail.com>
2021-07-06 10:05:14 +0800

    add code about metaclass

---
+++ books/pc/ch9/ch9_13_2.py
@@ -0,0 +1,9 @@
+class Spam:
+    def __init__(self, name):
+        self.name = name
+
+
+if __name__ == '__main__':
+    a = Spam('Guido')
+    b = Spam('Felix')
+
---
+++ books/pc/ch9/ch9_13_2_2.py
@@ -0,0 +1,15 @@
+class NoInstances(type):
+    def __call__(cls, *args, **kwargs):
+        raise TypeError('cannot instantiate directly')
+
+
+class Spam(metaclass=NoInstances):
+    @staticmethod
+    def grok(x):
+        print('spam.grok')
+
+
+if __name__ == '__main__':
+    Spam.grok(87)
+    s = Spam()  # error
+
