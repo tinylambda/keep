8b78bbd5cf77c0aeb8e87cafa7c10ce91d0f5ab7
Felix <tinylambda@gmail.com>
2021-07-06 19:05:27 +0800

    code about argparse

---
+++ module_argparse/argparse_arguments.py
@@ -0,0 +1,7 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='example with optional arguments')
+parser.add_argument('count', action='store', type=int)
+parser.add_argument('units', action='store')
+print(parser.parse_args())
+
---
+++ module_argparse/argparse_long.py
@@ -0,0 +1,9 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='example with long option name')
+parser.add_argument('--noarg', action='store_true', default=False)
+parser.add_argument('--witharg', action='store', dest='witharg')
+parser.add_argument('--witharg2', action='store', default='witharg2', type=int)
+
+print(parser.parse_args(['--noarg', '--witharg', 'val', '--witharg2=3']))
+
