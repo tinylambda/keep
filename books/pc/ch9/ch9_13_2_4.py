e5fd3cc4cfba7d308b520bf7a4cea12ca7fe4ba4
Felix <tinylambda@gmail.com>
2021-07-06 10:05:14 +0800

    add code about metaclass

---
+++ books/pc/ch9/ch9_13_2_4.py
@@ -0,0 +1,31 @@
+import weakref
+
+
+class Cached(type):
+    def __init__(cls, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        cls.__cache = weakref.WeakValueDictionary()
+
+    def __call__(cls, *args, **kwargs):
+        if args in cls.__cache:
+            return cls.__cache[args]
+        else:
+            obj = super().__call__(*args)
+            cls.__cache[args] = obj
+            return obj
+
+
+class Spam(metaclass=Cached):
+    def __init__(self, name):
+        print('creating spam({!r})'.format(name))
+        self.name = name
+
+
+if __name__ == '__main__':
+    a = Spam('Guido')
+    b = Spam('Felix')
+    c = Spam('Guido')
+
+    print(a is b)
+    print(a is c)
+
