62cb9fd3a0f1c790204bfb9aecea6622c692cf0e
Felix Pan <tinylambda@gmail.com>
2021-07-08 00:39:10 +0800

    use signature

---
+++ books/pc/ch9/ch9_16_3.py
@@ -0,0 +1,36 @@
+from inspect import Signature, Parameter
+
+
+def make_sig(*names):
+    params = [Parameter(name, Parameter.POSITIONAL_OR_KEYWORD) for name in names]
+    return Signature(params)
+
+
+class StructureMeta(type):
+    def __new__(mcs, clsname, bases, clsdict):
+        clsdict['__signature__'] = make_sig(*clsdict.get('_fields', []))
+        return super().__new__(mcs, clsname, bases, clsdict)
+
+
+class Structure(metaclass=StructureMeta):
+    _fields = []
+
+    def __init__(self, *args, **kwargs):
+        bound_values = self.__signature__.bind(*args, **kwargs)
+        for name, value in bound_values.arguments.items():
+            setattr(self, name, value)
+
+
+class Stock(Structure):
+    _fields = ['name', 'shares', 'price']
+
+
+class Point(Structure):
+    _fields = ['x', 'y']
+
+
+if __name__ == '__main__':
+    import inspect
+    print(inspect.signature(Stock))
+    print(inspect.signature(Point))
+
