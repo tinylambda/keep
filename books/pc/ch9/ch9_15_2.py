32ae744c78565c7681bb99eae33f9bf36914a936
Felix Pan <tinylambda@gmail.com>
2021-07-07 23:19:41 +0800

    metaclass example

---
+++ books/pc/ch9/ch9_15_2.py
@@ -0,0 +1,12 @@
+from abc import ABCMeta, abstractmethod
+
+
+class IStream(metaclass=ABCMeta):
+    @abstractmethod
+    def read(self, maxsize=None):
+        pass
+
+    @abstractmethod
+    def write(self, data):
+        pass
+
