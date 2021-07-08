27d1fcee248e518a235a5b0309c4f959fdc9a5d4
Felix <tinylambda@gmail.com>
2021-07-08 20:19:19 +0800

    more about argparse

---
+++ module_argparse/argparse_custom_help.py
@@ -0,0 +1,13 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='short sample app', add_help=True)
+parser.add_argument('-a', action='store_true', default=False)
+parser.add_argument('-b', action='store', dest='b')
+parser.add_argument('-c', action='store', dest='c', type=int)
+
+print('print_usage output:')
+parser.print_usage()
+
+print('print_help output:')
+parser.print_help()
+
---
+++ module_argparse/argparse_raw_description_help_formatter.py
@@ -0,0 +1,17 @@
+import argparse
+
+parser = argparse.ArgumentParser(add_help=True, formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+description
+    not
+        wrapped
+''', epilog='''
+epilog
+    not
+    wrapped''')
+
+parser.add_argument('-a', action='store_true', help='''argument
+help is
+wrapped''')
+
+parser.print_help()
+
