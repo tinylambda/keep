2cbba390b80f1a6a615409fa6fa02246041c3dc2
Felix <tinylambda@gmail.com>
2021-07-10 00:36:59 +0800

    add cmd

---
+++ module_cmd/cmd_arguments.py
@@ -0,0 +1,20 @@
+import cmd
+
+
+class HelloWorld(cmd.Cmd):
+    def do_greet(self, person):
+        if person:
+            print('hi', person)
+        else:
+            print('hi')
+
+    def do_EOF(self, line):
+        return True
+
+    def postloop(self) -> None:
+        print()
+
+
+if __name__ == '__main__':
+    HelloWorld().cmdloop()
+
---
+++ module_cmd/cmd_attributes.py
@@ -0,0 +1,24 @@
+import cmd
+
+
+class HelloWorld(cmd.Cmd):
+    prompt = 'prompt: '
+    intro = 'Simple command processor example.'
+
+    doc_header = 'doc_header'
+    misc_header = 'misc_header'
+    undoc_header = 'undoc_header'
+
+    ruler = '-'
+
+    def do_prompt(self, line):
+        """change the interactive prompt"""
+        self.prompt = line + ':'
+
+    def do_EOF(self, line):
+        return True
+
+
+if __name__ == '__main__':
+    HelloWorld().cmdloop()
+
