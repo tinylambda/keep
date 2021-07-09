86d28deec011e9319624987d1b33e8ba8a72205f
Felix <tinylambda@gmail.com>
2021-07-09 11:26:22 +0800

    more about argparse

---
+++ module_argparse/argparse_subparsers.py
@@ -0,0 +1,22 @@
+import argparse
+
+
+parser = argparse.ArgumentParser()
+
+subparsers = parser.add_subparsers(help='commands')
+
+list_parser = subparsers.add_parser('list', help='list contents')
+list_parser.add_argument('dirname', action='store', help='directory to list')
+
+create_parser = subparsers.add_parser('create', help='create a directory')
+create_parser.add_argument('driname', action='store', help='new directory to create')
+create_parser.add_argument('--readonly', default=False, action='store_true',
+                           help='set permissions to prevent writing to the directory')
+delete_parser = subparsers.add_parser('delete', help='remove a directory')
+delete_parser.add_argument('dirname', action='store', help='the directory to remove')
+delete_parser.add_argument('--recursive', '-r', default=False, action='store_true',
+                           help='remove the contents of the directory, too')
+print(parser.parse_args())
+
+# python argparse_subparsers.py -h
+# python argparse_subparsers.py list -h
--- module_argparse/argparse_uses_parent.py
+++ module_argparse/argparse_uses_parent.py
@@ -5,3 +5,5 @@ parser = argparse.ArgumentParser(parents=[argparse_parent_base.parser])
 parser.add_argument('--local-arg', action='store_true', default=False)
 print(parser.parse_args())
 
+# python argparse_uses_parent.py -h
+
