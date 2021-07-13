1ab3d82f2f9b4188f77f0a790c8d087be3957995
Felix <tinylambda@gmail.com>
2021-07-13 10:21:34 +0800

    add code about excel style

---
+++ module_openpyxl/openpyxl_simple_style_reuse.py
@@ -0,0 +1,20 @@
+import pathlib
+
+from openpyxl import Workbook
+from openpyxl.styles import NamedStyle, Font, Border, Side, Alignment
+
+if __name__ == '__main__':
+    header = NamedStyle(name='header')
+    header.font = Font(bold=True)
+    header.border = Border(bottom=Side(border_style='thin'))
+    header.alignment = Alignment(horizontal='center', vertical='center')
+
+    workbook = Workbook()
+    sheet = workbook.create_sheet('Sample Sheet')
+    sheet.append(('this', 'is', 'a', 'good', 'thing'))
+    header_row = sheet[1]
+    for cell in header_row:
+        cell.style = header
+
+    filepath = pathlib.Path.home().joinpath('Downloads/test2.xlsx')
+    workbook.save(filepath)
