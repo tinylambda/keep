6d730d70f63e48c8f82c5be145394f6a9fee59f4
Felix <tinylambda@gmail.com>
2021-07-16 14:58:32 +0800

    more on excel

---
+++ module_openpyxl/openpyxl_simple_chart_more.py
@@ -0,0 +1,47 @@
+import pathlib
+import random
+
+from openpyxl import Workbook
+from openpyxl.chart import LineChart, Reference
+
+
+if __name__ == '__main__':
+    workbook = Workbook()
+    sheet = workbook.create_sheet('Sample Sheet')
+    rows = [
+        ["", "January", "February", "March", "April",
+         "May", "June", "July", "August", "September",
+         "October", "November", "December"],
+        [1, ],
+        [2, ],
+        [3, ],
+    ]
+
+    for row in rows:
+        sheet.append(row)
+
+    for row in sheet.iter_rows(min_row=2, max_row=4, min_col=2, max_col=13):
+        for cell in row:
+            cell.value = random.randrange(5, 100)
+
+    chart = LineChart()
+    data = Reference(
+        worksheet=sheet,
+        min_row=2,
+        max_row=4,
+        min_col=1,
+        max_col=13
+    )
+    cats = Reference(worksheet=sheet, min_row=1, max_row=1, min_col=2, max_col=13)
+    chart.add_data(data, from_rows=True, titles_from_data=True)
+    chart.set_categories(cats)
+    chart.x_axis.title = "Months"
+    chart.y_axis.title = "Sales (per unit)"
+    # You can play with this by choosing any number between 1 and 48
+    chart.style = 24
+
+    sheet.add_chart(chart, 'C6')
+
+    filepath = pathlib.Path.home().joinpath('Downloads/test12.xlsx')
+    workbook.save(filepath)
+
