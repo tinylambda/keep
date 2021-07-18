60c7bf1375d44db36a5635241c86fb32da603514
Felix Pan <tinylambda@gmail.com>
2021-07-19 01:08:51 +0800

    add code about http client

---
+++ books/pc/ch11/ch11_1_2_4.py
@@ -0,0 +1,21 @@
+import json
+import pprint
+
+import requests
+
+
+if __name__ == '__main__':
+    url = 'http://httpbin.org/post'
+
+    params = {
+        'name1': 'value1',
+        'name2': 'value2',
+    }
+
+    headers = {
+        'User-Agent': 'good/felix',
+        'Content-Type': 'application/json;charset=utf-8',
+    }
+
+    resp = requests.post(url, data=json.dumps(params), headers=headers)
+    pprint.pprint(resp.json())
