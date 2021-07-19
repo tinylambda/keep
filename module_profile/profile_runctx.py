61b8acfa32888990e32654f3ef8af529179aec23
Felix <tinylambda@gmail.com>
2021-07-19 17:28:28 +0800

    add code about profile

---
+++ module_profile/profile_runctx.py
@@ -0,0 +1,6 @@
+import profile
+from profile_fibonacci_memoized import fib, fib_seq
+
+
+if __name__ == '__main__':
+    profile.runctx('print(fib_seq(n)); print()', globals(), {'n': 20})
---
+++ module_profile/profile_stats_restricted.py
@@ -0,0 +1,14 @@
+import cProfile as profile
+import pstats
+from profile_fibonacci_memoized import fib, fib_seq
+
+stats = pstats.Stats('profile_stats_0.stats')
+for i in range(1, 5):
+    stats.add(f'profile_stats_{i}.stats')
+
+# cleanup filenames for the report
+stats.strip_dirs()
+
+# sort the statistics by the cumulative time spent in the function
+stats.sort_stats('cumulative')
+stats.print_stats(r'\(fib')
