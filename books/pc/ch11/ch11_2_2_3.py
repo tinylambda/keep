30ab34c281ff61430291c01617083da91d19fca2
Felix Pan <tinylambda@gmail.com>
2021-07-21 01:49:12 +0800

    simple tcp server

---
+++ books/pc/ch11/ch11_2_2_3.py
@@ -0,0 +1,22 @@
+from socketserver import StreamRequestHandler, TCPServer
+
+
+class EchoHandler(StreamRequestHandler):
+    # def setup(self) -> None:
+    #     super().setup()
+    #     print('setup')
+
+    def handle(self) -> None:
+        print('got connection from', self.client_address)
+        for line in self.rfile:
+            print(line, type(line))
+            self.wfile.write(line)
+
+    # def finish(self) -> None:
+    #     super().finish()
+    #     print('finish')
+
+
+if __name__ == '__main__':
+    serv = TCPServer(('', 20000), EchoHandler)
+    serv.serve_forever()
