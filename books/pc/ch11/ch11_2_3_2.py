947c83ff36eb787434fc27bc4952db2ea9f93d08
Felix Pan <tinylambda@gmail.com>
2021-07-24 22:27:31 +0800

    more on tcp

---
+++ books/pc/ch11/ch11_2_3_2.py
@@ -0,0 +1,20 @@
+from socketserver import StreamRequestHandler, ThreadingTCPServer, ForkingTCPServer, TCPServer
+
+
+class EchoHandler(StreamRequestHandler):
+    def handle(self) -> None:
+        print('got connection from', self.client_address)
+        for line in self.rfile:
+            print(line, type(line))
+            self.wfile.write(line)
+
+
+if __name__ == '__main__':
+    from threading import Thread
+    NWORKERS = 2
+    serv = TCPServer(('', 20000), EchoHandler)
+    for n in range(NWORKERS):
+        t = Thread(target=serv.serve_forever)
+        t.daemon = True
+        t.start()
+    serv.serve_forever()
---
+++ books/pc/ch11/ch11_2_3_3.py
@@ -0,0 +1,19 @@
+import socket
+from socketserver import StreamRequestHandler, TCPServer
+
+
+class EchoHandler(StreamRequestHandler):
+    def handle(self) -> None:
+        print('got connection from', self.client_address)
+        for line in self.rfile:
+            print(line, type(line))
+            self.wfile.write(line)
+
+
+if __name__ == '__main__':
+    serv = TCPServer(('', 20000), EchoHandler, bind_and_activate=False)
+    serv.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+
+    serv.server_bind()
+    serv.server_activate()
+    serv.serve_forever()
