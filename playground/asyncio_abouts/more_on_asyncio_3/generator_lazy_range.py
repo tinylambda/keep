9bff86562bf9d526a6cee8c6b86770fc93a5643d
Felix <tinylambda@gmail.com>
2021-07-27 21:11:17 +0800

    more on asyncio

---
+++ playground/asyncio_abouts/more_on_asyncio_3/generator_lazy_range.py
@@ -0,0 +1,15 @@
+def lazy_range(up_to):
+    index = 0
+
+    def gratuitous_refactor():
+        nonlocal index
+        while index < up_to:
+            yield index
+            index += 1
+
+    yield from gratuitous_refactor()
+
+
+if __name__ == '__main__':
+    for i in lazy_range(10):
+        print(i)
---
+++ playground/asyncio_abouts/more_on_asyncio_3/generator_return_value.py
@@ -0,0 +1,22 @@
+def bottom():
+    return (yield 42)
+
+
+def middle():
+    return (yield from bottom())
+
+
+def top():
+    return (yield from middle())
+
+
+if __name__ == '__main__':
+    gen = top()
+    value = next(gen)
+    print(value)
+
+    try:
+        value = gen.send(value * 2)
+    except StopIteration as e:
+        value = e.value
+    print(value)
