af01b682557803773152052dae8710fcd99af25c
Felix Pan <tinylambda@gmail.com>
2021-08-02 22:31:51 +0800

    more on ipc

--- books/pc/ch11/ch11_11_3_3.py
+++ books/pc/ch11/ch11_11_3_3.py
@@ -1,29 +1,36 @@
-from multiprocessing.connection import Listener
-from multiprocessing.reduction import send_handle
 import socket
+import struct
+
+
+def send_fd(sock, fd):
+    """send a single file descriptor"""
+    sock.sendmsg([b'x'], [(socket.SOL_SOCKET, socket.SCM_RIGHTS, struct.pack('i', fd))])
+    ack = sock.recv(2)
+    assert ack == b'OK'
 
 
 def server(work_address, port):
-    # wait for the worker to connect
-    work_serv = Listener(work_address, authkey=b'peekaboo')
-    worker = work_serv.accept()
-    worker_pid = worker.recv()
+    work_serv = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    work_serv.bind(work_address)
+    work_serv.listen(1)
+    worker, addr = work_serv.accept()
 
     # now run a TCP/IP server and send clients to worker
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
     s.bind(('', port))
     s.listen(1)
+
     while True:
         client, addr = s.accept()
         print('server: got connection from', addr)
-        send_handle(worker, client.fileno(), worker_pid)
+        send_fd(worker, client.fileno())
         client.close()
 
 
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) != 3:
+    if (len(sys.argv)) != 3:
         print('usage: server.py server_address port', file=sys.stderr)
         raise SystemExit(1)
     server(sys.argv[1], int(sys.argv[2]))
