ddbe58e943ed197a0fcd771aa0bb29035b917971
Felix <tinylambda@gmail.com>
2021-08-06 14:10:23 +0800

    transfer big array data

---
+++ books/pc/ch11/ch11_13_2_2.py
@@ -0,0 +1,19 @@
+from socket import *
+import numpy
+
+
+def recv_into(arr, source):
+    view = memoryview(arr).cast('B')
+    while len(view):
+        nrecv = source.recv_into(view)
+        view = view[nrecv:]
+
+
+if __name__ == '__main__':
+    c = socket(AF_INET, SOCK_STREAM)
+    c.connect(('localhost', 25000))
+
+    a = numpy.zeros(shape=5000000, dtype=float)
+    print(a[:10])
+    recv_into(a, c)
+    print(a[:10])
---
+++ playground/asyncio_abouts/more_on_asyncio_4/sample_2_2.py
@@ -0,0 +1,55 @@
+import logging
+import queue
+import random
+import string
+import time
+
+import attr
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s,%(msecs)d %(levelname)s: %(message)s',
+    datefmt='%H:%M:%S',
+)
+
+
+@attr.s
+class PubSubMessage:
+    instance_name = attr.ib()
+    message_id = attr.ib(repr=False)
+    hostname = attr.ib(repr=False, init=False)
+
+    def __attrs_post_init__(self):
+        self.hostname = f'{self.instance_name}.example.net'
+
+
+def publish(q, n):
+    choices = string.ascii_lowercase + string.digits
+    for x in range(1, n+1):
+        host_id = ''.join(random.choices(choices, k=4))
+        instance_name = f'cattle-{host_id}'
+        msg = PubSubMessage(message_id=x, instance_name=instance_name)
+        # publish an item
+        q.put(msg)
+        logging.info(f'published {x} of {n} messages')
+    q.put(None)
+
+
+def consume(q):
+    while True:
+        msg = q.get()
+        if msg is None:
+            break
+        logging.info(f'consumed {msg}')
+        time.sleep(random.random())
+
+
+def main():
+    q = queue.Queue()
+    publish(q, 5)
+    consume(q)
+
+
+if __name__ == '__main__':
+    main()
