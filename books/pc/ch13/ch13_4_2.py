9876134f2b7db9a26d0e608dbd993a325fc7f7d4
Felix Pan <tinylambda@gmail.com>
2021-08-11 01:03:15 +0800

    more on termial

---
+++ books/pc/ch13/ch13_4_2.py
@@ -0,0 +1,7 @@
+import getpass
+
+
+user = getpass.getuser()
+passwd = getpass.getpass()
+
+print(user, passwd)
---
+++ books/pc/ch13/ch13_5_2.py
@@ -0,0 +1,6 @@
+import os
+
+
+sz = os.get_terminal_size()
+print(sz)
+print(sz.columns, sz.lines)
