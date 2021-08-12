450b0345b2ef42c5480e08a018f4f1908364d599
Felix Pan <tinylambda@gmail.com>
2021-08-12 16:25:58 +0800

    more on cmd program

---
+++ books/pc/ch13/ch13_11_2.py
@@ -0,0 +1,24 @@
+import logging
+
+
+def main():
+    logging.basicConfig(
+        filename='app.log',
+        level=logging.WARNING,
+    )
+
+    hostname = 'www.python.org'
+    item = 'spam'
+    filename = 'data.csv'
+    mode = 'r'
+
+    # example logging calls (insert into your program)
+    logging.critical('host %s unknown', hostname)
+    logging.error('could not file item %r', item)
+    logging.warning('feature is depreciated')
+    logging.info('opening file %r mode %r', filename, mode)
+    logging.debug('got here')
+
+
+if __name__ == '__main__':
+    main()
