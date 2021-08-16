af1ffe09e047cb6cb8159d1992d18dc14e38279c
Felix <tinylambda@gmail.com>
2021-08-16 15:34:28 +0800

    test logging stacklevel

--- module_logging/logging_config_dict.py
+++ module_logging/logging_config_dict.py
@@ -7,9 +7,17 @@ with open(file='logging.yaml', mode='r', encoding='utf-8') as f:
     logging_yaml = yaml.load(stream=f, Loader=yaml.FullLoader)
     logging.config.dictConfig(config=logging_yaml)
 
-logger = logging.getLogger('simpleExample')
+
+class LogProxy:
+    def __init__(self, logtype):
+        self.logger = logging.getLogger(logtype)
+
+    def record_role(self, role):
+        self.logger.info("role is %s", role, stacklevel=2)
+
 
 if __name__ == '__main__':
+    logger = logging.getLogger('simpleExample')
     logger.debug('debug message')
     logger.info('info message')
     logger.warning('warn message')
