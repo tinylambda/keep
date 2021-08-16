bd5d66d4fea8b54198b3f731c38d4027a4828eaf
Felix <tinylambda@gmail.com>
2021-08-16 15:34:28 +0800

    test logging stacklevel

---
+++ module_logging/logging_stacklevel.py
@@ -0,0 +1,9 @@
+from module_logging.logging_config_dict import LogProxy
+
+
+if __name__ == '__main__':
+    lp = LogProxy("mytestlogger")
+    lp.record_role('felix')
+
+    logger = lp.logger;
+    logger.info("felix 2")
