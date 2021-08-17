aa03f084b3a5153970ca9d942f1f2698dcd78b58
Felix <tinylambda@gmail.com>
2021-08-17 17:00:44 +0800

    start python essential reference

---
+++ books/per/__init__.py
@@ -0,0 +1,1 @@
+"""<<python essential reference>>"""
---
+++ books/per/ch1/ch1_1_1.py
@@ -0,0 +1,24 @@
+principal = 1000
+rate = 0.05
+numyears = 5
+year = 1
+
+# while year <= numyears:
+#     principal = principal * (1 + rate)
+#     print(year, principal)
+#     year += 1
+
+# while year <= numyears:
+#     principal = principal * (1 + rate)
+#     print('%3d %0.2f' % (year, principal))
+#     year += 1
+
+# while year <= numyears:
+#     principal = principal * (1 + rate)
+#     print(format(year, '3d'), format(principal, '0.2f'))
+#     year += 1
+
+while year <= numyears:
+    principal = principal * (1 + rate)
+    print('{0:3d} {1:0.2f}'.format(year, principal))
+    year += 1
