34f62d4a2f87a53e9983d6c512f2d698e704ed28
Felix Pan <tinylambda@gmail.com>
2021-08-22 22:26:02 +0800

    call sync code in async code

---
+++ playground/asyncio_abouts/more_on_asyncio_4/sample_19.py
@@ -0,0 +1,61 @@
+import logging
+import queue
+import random
+import string
+import time
+
+import attr
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s,%(msecs)s %(levelname)s: %(message)s',
+    datefmt='%H:%M:%S'
+)
+
+
+@attr.s
+class PubSubMessage:
+    instance_name = attr.ib()
+    message_id = attr.ib(repr=False)
+    hostname = attr.ib(repr=False, init=False)
+
+    def __attrs_post_init__(self):
+        self.hostname = f'{self.instance_name}.example.net'
+
+
+def publish(q, n):
+    choices = string.ascii_lowercase + string.digits
+
+    for x in range(1, n + 1):
+        host_id = ''.join(random.choices(choices, k=4))
+        instance_name = f'cattle-{host_id}'
+        msg = PubSubMessage(message_id=x, instance_name=instance_name)
+        # publish an item
+        q.put(msg)
+        logging.info(f'published {x} of {n} messages')
+    q.put(None)
+
+
+def consume(q):
+    while True:
+        # wait for an item from the publisher
+        msg = q.get()
+
+        # the publisher emits None to indicate that it is done
+        if msg is None:
+            break
+
+        # process the message
+        logging.info(f'consumed {msg}')
+        # simulate I.O operation using sleep
+        time.sleep(random.random())
+
+
+def main():
+    q = queue.Queue()
+    publish(q, 5)
+    consume(q)
+
+
+if __name__ == '__main__':
+    main()
---
+++ playground/asyncio_abouts/more_on_asyncio_4/sample_21.py
@@ -0,0 +1,89 @@
+import asyncio
+import concurrent.futures
+import logging
+import queue
+import random
+import string
+import time
+import uuid
+
+import attr
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s,%(msecs)s %(levelname)s: %(message)s',
+    datefmt='%H:%M:%S'
+)
+
+
+@attr.s
+class PubSubMessage:
+    instance_name = attr.ib()
+    message_id = attr.ib(repr=False)
+    hostname = attr.ib(repr=False, init=False)
+
+    def __attrs_post_init__(self):
+        self.hostname = f'{self.instance_name}.example.net'
+
+
+def publish_sync(q):
+    choices = string.ascii_lowercase + string.digits
+
+    while True:
+        msg_id = str(uuid.uuid4())
+        host_id = ''.join(random.choices(choices, k=4))
+        instance_name = f'cattle-{host_id}'
+        msg = PubSubMessage(message_id=msg_id, instance_name=instance_name)
+        # publish an item
+        q.put(msg)
+        logging.info(f'published {msg}')
+        # simulate randomness of publishing messages
+        time.sleep(random.random())
+
+
+def consume_sync(q):
+    while True:
+        # wait for an item from the publisher
+        msg = q.get()
+        # process the message
+        logging.info(f'consumed {msg}')
+        # substitute for handling a message
+        time.sleep(random.random())
+
+
+async def publish(executor, q):
+    logging.info('starting publisher')
+    loop = asyncio.get_event_loop()
+    futures = [
+        loop.run_in_executor(executor, publish_sync, q)
+        for i in range(5)
+    ]
+    asyncio.ensure_future(asyncio.gather(*futures, return_exceptions=True))
+
+
+async def consume(executor, q):
+    logging.info('starting consumer')
+    loop = asyncio.get_event_loop()
+    futures = [
+        loop.run_in_executor(executor, consume_sync, q)
+        for i in range(5)
+    ]
+    asyncio.ensure_future(asyncio.gather(*futures, return_exceptions=True))
+
+
+def main():
+    executor = concurrent.futures.ThreadPoolExecutor()
+    loop = asyncio.get_event_loop()
+    q = queue.Queue()
+
+    try:
+        loop.create_task(publish(executor, q))
+        loop.create_task(consume(executor, q))
+        loop.run_forever()
+    finally:
+        loop.close()
+        logging.info('successfully shutdown the Mayhem service.')
+
+
+if __name__ == '__main__':
+    main()
