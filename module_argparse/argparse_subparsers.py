883f869be6c8e48cf1c215a647789652a85e794d
Felix <tinylambda@gmail.com>
2021-08-25 11:09:24 +0800

    use dest to identify which subparser used

--- module_argparse/argparse_subparsers.py
+++ module_argparse/argparse_subparsers.py
@@ -3,7 +3,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-subparsers = parser.add_subparsers(help='commands')
+subparsers = parser.add_subparsers(help='commands', dest='command')
 
 list_parser = subparsers.add_parser('list', help='list contents')
 list_parser.add_argument('dirname', action='store', help='directory to list')
@@ -16,7 +16,10 @@ delete_parser = subparsers.add_parser('delete', help='remove a directory')
 delete_parser.add_argument('dirname', action='store', help='the directory to remove')
 delete_parser.add_argument('--recursive', '-r', default=False, action='store_true',
                            help='remove the contents of the directory, too')
-print(parser.parse_args())
+
+ns = parser.parse_args()
+print(ns)
+print()
 
 # python argparse_subparsers.py -h
 # python argparse_subparsers.py list -h
