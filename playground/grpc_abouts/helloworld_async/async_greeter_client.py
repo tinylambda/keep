5f1db3d7c8f76c6a1095f6cc18fe43fc6905949a
Felix Pan <tinylambda@gmail.com>
2021-08-30 00:26:41 +0800

    grpc use asyncio

---
+++ playground/grpc_abouts/helloworld_async/async_greeter_client.py
@@ -0,0 +1,19 @@
+import asyncio
+
+import grpc.aio
+
+import helloworld_pb2_grpc
+import helloworld_pb2
+
+
+async def run():
+    async with grpc.aio.insecure_channel('localhost:50051') as channel:
+        stub = helloworld_pb2_grpc.GreeterStub(channel)
+        response = await stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+        print('greeter client received: ', response.message)
+        response = await stub.SayHelloAgain(helloworld_pb2.HelloRequest(name='you'))
+        print('greeter client received: ', response.message)
+
+
+if __name__ == '__main__':
+    asyncio.run(run())
