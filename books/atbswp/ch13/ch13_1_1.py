a0274f685ead7129b86a73e16e7a97d20ea67a9a
Felix Pan <tinylambda@gmail.com>
2021-09-05 23:29:43 +0800

    edit pdf files

---
+++ books/atbswp/ch13/ch13_1_1.py
@@ -0,0 +1,10 @@
+import PyPDF3
+
+if __name__ == '__main__':
+    pdf_file = '/home/felix/Downloads/The Definitive Guide to Masonite.pdf'
+    pdf_file_obj = open(pdf_file, 'rb')
+    pdf_reader = PyPDF3.PdfFileReader(pdf_file_obj)
+    print(pdf_reader.getNumPages())
+
+    page_obj = pdf_reader.getPage(0)
+    print(page_obj.extractText())
