07d62f3000dd9b03bcbc0e43a04fb82df6d61ca5
Felix <tinylambda@gmail.com>
2021-09-07 19:46:14 +0800

    grpc dynamic protos

---
+++ playground/grpc_abouts/helloworld_nocodegen/greeter_client.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+import logging
+import grpc
+import grpc.experimental
+
+
+protos = grpc.protos('helloworld.proto')
+services = grpc.services('helloworld.proto')
+
+response = services.Greeter.SayHello(protos.HelloRequest(name='you'),
+                                     'localhost:50051',
+                                     insecure=True)
+print(f'greeter client received: {response.message}')
