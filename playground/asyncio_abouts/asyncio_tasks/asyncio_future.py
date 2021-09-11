a70b0631bdd1b9e5a9abb0441bf054c1dbabb986
Felix <tinylambda@gmail.com>
2021-09-11 21:27:34 +0800

    sync code

---
+++ playground/asyncio_abouts/asyncio_tasks/asyncio_future.py
@@ -0,0 +1,34 @@
+import asyncio
+import logging
+
+logging.basicConfig(
+    level=logging.INFO
+)
+
+
+async def cost_long_time(s: int):
+    await asyncio.sleep(s)
+    logging.info('done')
+
+
+async def raise_exception():
+    raise RuntimeError('just for example')
+
+
+async def do():
+    # task = asyncio.create_task(cost_long_time(10))
+    # logging.info('result: %s', task.result())
+
+    task = asyncio.create_task(raise_exception())
+    logging.info('result: %s', task.result())
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(do())
+    loop.close()
+    logging.info('main done')
+
+
+if __name__ == '__main__':
+    main()
