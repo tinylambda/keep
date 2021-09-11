8fbb9518e3bc8169160c4ce9260ffd7ed2becb57
Felix <tinylambda@gmail.com>
2021-09-11 21:57:03 +0800

    read image using pillow

---
+++ books/atbswp/ch17/ch17_1_1.py
@@ -0,0 +1,23 @@
+import logging
+import pprint
+import sys
+
+from PIL import ImageColor
+
+logging.basicConfig(level=logging.INFO, stream=sys.stderr)
+
+if __name__ == '__main__':
+    logging.info("show colors: ")
+    logging.info(ImageColor.getcolor('red', 'RGBA'))
+    logging.info(ImageColor.getcolor('RED', 'RGBA'))
+    logging.info(ImageColor.getcolor('Black', 'RGBA'))
+    logging.info(ImageColor.getcolor('chocolate', 'RGBA'))
+    logging.info(ImageColor.getcolor('cornflowerblue', 'RGBA'))
+
+    try:
+        logging.info(ImageColor.getcolor('notexists', 'RGBA'))
+    except ValueError as e:
+        logging.error("color not exists", e)
+
+    logging.info('show all known colors')
+    pprint.pprint(ImageColor.colormap)
---
+++ books/atbswp/ch17/ch17_2_1.py
@@ -0,0 +1,12 @@
+import logging
+import sys
+
+from PIL import Image
+
+logging.basicConfig(level=logging.INFO, stream=sys.stderr)
+
+
+if __name__ == '__main__':
+    cat_image = Image.open('cat.jpeg')
+    logging.info(cat_image)
+    
