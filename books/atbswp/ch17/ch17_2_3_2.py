e708337bb7ffd375b9a65af32c8abdbcba782aaf
Felix <tinylambda@gmail.com>
2021-09-12 23:39:56 +0800

    more on picture operations

---
+++ books/atbswp/ch17/ch17_2_3_2.py
@@ -0,0 +1,21 @@
+import logging
+import sys
+
+from PIL import Image
+
+logging.basicConfig(level=logging.INFO, stream=sys.stderr)
+
+
+if __name__ == '__main__':
+    cat_image: Image.Image = Image.open('cat.jpeg')
+    cat_width, cat_height = cat_image.size
+    cropped_image: Image.Image = cat_image.crop((335, 345, 565, 560))
+    face_width, face_height = cropped_image.size
+    copied_image = cat_image.copy()
+
+    for left in range(0, cat_width, face_width):
+        for top in range(0, cat_height, face_height):
+            logging.info('left: %s, top: %s', left, top)
+            copied_image.paste(cropped_image, (left, top))
+
+    copied_image.save('tiled.png')
---
+++ books/atbswp/ch17/ch17_2_4.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+
+from PIL import Image
+
+logging.basicConfig(level=logging.INFO, stream=sys.stderr)
+
+
+if __name__ == '__main__':
+    cat_image: Image.Image = Image.open('cat.jpeg')
+    width, height = cat_image.size
+
+    image_half: Image.Image = cat_image.resize((width // 2, height // 2))
+    image_half.save('half.png')
+
+    image_higher: Image.Image = cat_image.resize((width, height + 300))
+    image_higher.save('higher.png')
