7a607a28e2566e5f970805aeebcffea07d8e85f3
Felix <tinylambda@gmail.com>
2021-09-24 00:54:59 +0800

    more on attrs

---
+++ module_attrs/attrs_init.py
@@ -0,0 +1,32 @@
+import logging
+import sys
+from collections import namedtuple
+
+import attr
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+class PointBad:
+    def __init__(self, database_row):
+        self.x = database_row.x
+        self.y = database_row.y
+
+
+@attr.s
+class PointGood:
+    x = attr.ib()
+    y = attr.ib()
+
+    @classmethod
+    def from_row(cls, row):
+        return cls(row.x, row.y)
+
+
+if __name__ == '__main__':
+    Row = namedtuple('Row', 'x,y')
+    pb = PointBad(Row(x=1, y=2))
+    logging.info('x = %s, y = %s', pb.x, pb.y)
+
+    pg = PointGood.from_row(Row(x=1, y=2))
+    logging.info('%s', pg)
---
+++ module_attrs/attrs_init_private.py
@@ -0,0 +1,17 @@
+import inspect
+import logging
+import sys
+
+import attr
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+@attr.s
+class C:
+    _x = attr.ib()
+
+
+if __name__ == '__main__':
+    signature = inspect.signature(C.__init__)
+    logging.info('%s', signature)
