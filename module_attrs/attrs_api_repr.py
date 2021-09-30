e9b7ba98bba95b662ac9187188323c6d69fa917d
Felix <tinylambda@gmail.com>
2021-10-01 01:13:11 +0800

    more on attrs api

---
+++ module_attrs/attrs_api_repr.py
@@ -0,0 +1,23 @@
+import logging
+import sys
+
+import attr
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+@attr.s(repr=False)
+class A:
+    x = attr.ib(type=int)
+
+
+@attr.s(repr=True)
+class B:
+    x = attr.ib(type=int)
+
+
+if __name__ == '__main__':
+    a = A(100)
+    b = B(100)
+    logging.info('%s', a)
+    logging.info('%s', b)
