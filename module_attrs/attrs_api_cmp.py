baef7fe069618fa8d36e77809dd7aec8451afd40
Felix <tinylambda@gmail.com>
2021-10-03 23:07:50 +0800

    add attrs api cmp

---
+++ module_attrs/attrs_api_cmp.py
@@ -0,0 +1,35 @@
+import logging
+import sys
+
+import attr
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+@attr.s(cmp=True)
+class C:
+    x = attr.ib()
+    y = attr.ib()
+
+
+@attr.s(cmp=False)
+class C2:
+    x = attr.ib()
+    y = attr.ib()
+
+
+if __name__ == '__main__':
+    c_1 = C(100, 100)
+    c_2 = C(100, 100)
+    c_3 = C(100, 200)
+
+    logging.info('%s', c_1 == c_2)
+    logging.info('%s', c_1 == c_3)
+
+    c_1 = C2(100, 100)
+    c_2 = C2(100, 100)
+    c_3 = C2(100, 200)
+
+    logging.info('%s', c_1 == c_2)
+    logging.info('%s', c_1 == c_3)
+
