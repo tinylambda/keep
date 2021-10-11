129f62265679f9e50e2ad558e345c55945cbce4e
Felix <tinylambda@gmail.com>
2021-10-11 22:29:34 +0800

    more on attrs ib

---
+++ module_attrs/attrs_ib_kw_only.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+
+import attr
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+@attr.s
+class C:
+    x = attr.ib(kw_only=True)
+    y = attr.ib()
+
+
+if __name__ == '__main__':
+    c = C(100, x=20)
+    logging.info('%s', c)
