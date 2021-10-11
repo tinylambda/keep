0e621c630a9b40c0dd0375a4dbd380723173bfd1
Felix <tinylambda@gmail.com>
2021-10-12 00:05:29 +0800

    attrs attribute

---
+++ module_attrs/attrs_attribute.py
@@ -0,0 +1,15 @@
+import logging
+import sys
+
+import attr
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+@attr.s
+class C:
+    x = attr.ib()
+
+
+if __name__ == '__main__':
+    logging.info('%s', attr.fields(C).x)  # Read-only representation of an attribute.
