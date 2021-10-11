340dc9371ad6994931879ef28b82701bb799888d
Felix <tinylambda@gmail.com>
2021-10-11 14:12:15 +0800

    get metadata in method

--- module_attrs/attrs_metadata.py
+++ module_attrs/attrs_metadata.py
@@ -10,6 +10,12 @@ logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 class C:
     x = attr.ib(metadata={'my_metadata': 1})
 
+    def hello(self):
+        logging.info('%s my value is %s', 'world', self.x)
+        logging.info('%s', attr.fields(self.__class__).x.metadata)
+
 
 if __name__ == '__main__':
     logging.info('%s', attr.fields(C).x.metadata)
+    c = C(100)
+    c.hello()
