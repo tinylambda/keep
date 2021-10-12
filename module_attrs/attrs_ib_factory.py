3efcd4f83feb49acdddd35f59a2105b8fd3e316f
Felix <tinylambda@gmail.com>
2021-10-12 09:50:49 +0800

    more on factory

--- module_attrs/attrs_ib_factory.py
+++ module_attrs/attrs_ib_factory.py
@@ -10,8 +10,13 @@ logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 class C:
     # Syntactic sugar for default=attr.Factory(factory).
     f = attr.ib(factory=list)
+    x = attr.ib(default=attr.Factory(list))
+    y = attr.ib(default=attr.Factory(lambda self: set(self.x), takes_self=True))
 
 
 if __name__ == '__main__':
-    c = C()
+    c = C(x=[1, 2, 3])
+    logging.info('%s', c)
     logging.info('%s', c.f)
+
+
