3fe2e9554a4acb1b4c7580cbdf9e147967e9e30f
Felix <tinylambda@gmail.com>
2021-10-22 00:32:00 +0800

    complete attrs validators

---
+++ module_attrs/attrs_validators_deep_mapping.py
@@ -0,0 +1,30 @@
+import logging
+import sys
+
+import attr
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+@attr.s
+class C:
+    x = attr.ib(validator=attr.validators.deep_mapping(
+        key_validator=attr.validators.instance_of(str),
+        value_validator=attr.validators.instance_of(int),
+        mapping_validator=attr.validators.instance_of(dict)
+    ))
+
+
+if __name__ == '__main__':
+    c = C(x={'a': 1, 'b': 2})
+    logging.info('1. %s', c)
+
+    try:
+        c = C(None)
+    except TypeError as e:
+        logging.error('type error', exc_info=e)
+
+    try:
+        c = C({'a': 1, 2: 3})
+    except TypeError as e:
+        logging.error('type error', exc_info=e)
