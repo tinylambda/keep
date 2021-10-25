16faee41987f3e6ac0e14ff38ccd9d2e5e018d94
Felix Pan <tinylambda@gmail.com>
2021-10-26 01:31:07 +0800

    zmq pub sub pattern

---
+++ module_zmq/weather_update/server.py
@@ -0,0 +1,24 @@
+import logging
+import random
+import sys
+import time
+
+import zmq
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+if __name__ == '__main__':
+    context = zmq.Context()
+    publisher = context.socket(zmq.PUB)
+
+    publisher.bind('tcp://*:5556')
+    publisher.bind('ipc://weather.ipc')
+
+    while True:
+        zipcode = random.randint(0, 100000)
+        temperature = random.randint(0, 215) - 80
+        relhumidity = random.randint(0, 50) + 10
+
+        message = f'{zipcode} {temperature} {relhumidity}'
+        publisher.send_multipart([f'{zipcode}'.encode(), message.encode()])
