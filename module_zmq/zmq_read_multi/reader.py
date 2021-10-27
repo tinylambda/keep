9f2fcfe5f07152094f8cd3b67586bb663a5b6272
Felix <tinylambda@gmail.com>
2021-10-27 11:07:36 +0800

    zmq nonblocking read

---
+++ module_zmq/zmq_read_multi/reader.py
@@ -0,0 +1,38 @@
+import logging
+import sys
+
+import zmq
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+"""
+1. start two publish servers:
+# python server.py py 5555
+# python server.py go 5556
+
+2. start this reader
+python reader.py
+"""
+
+if __name__ == '__main__':
+    context = zmq.Context()
+
+    py_reader = context.socket(zmq.SUB)
+    py_reader.connect('tcp://localhost:5555')
+    py_reader.setsockopt(zmq.SUBSCRIBE, b'')
+
+    go_reader = context.socket(zmq.SUB)
+    go_reader.connect('tcp://localhost:5556')
+    go_reader.setsockopt(zmq.SUBSCRIBE, b'')
+
+    while True:
+        try:
+            py_message = py_reader.recv(zmq.DONTWAIT)
+            logging.info('processing py message: %s', py_message)
+        except zmq.error.Again:
+            pass
+        try:
+            go_message = go_reader.recv(zmq.DONTWAIT)
+            logging.info('processing go message: %s', go_message)
+        except zmq.error.Again:
+            pass
---
+++ module_zmq/zmq_read_multi/server.py
@@ -0,0 +1,28 @@
+import datetime
+import logging
+import sys
+import time
+
+import zmq
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+if __name__ == '__main__':
+    name = sys.argv[1]
+    port = sys.argv[2]
+    logging.info('%s %s', name, port)
+
+    context = zmq.Context()
+    publisher = context.socket(zmq.PUB)
+
+    bind_to = f'tcp://*:{port}'
+    publisher.bind(bind_to)
+
+    while True:
+        ts = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        publisher.send(f'{name}-{port}: {ts}'.encode('utf-8'))
+        time.sleep(0.1)
+
+# python server.py py 5555
+# python server.py go 5556
