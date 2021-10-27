075da587749b4ffe337b15d3437bd64b87509ad1
Felix <tinylambda@gmail.com>
2021-10-27 15:39:00 +0800

    use zmq to implement load balance

---
+++ module_zmq/zmq_request_reply_extened/broker.py
@@ -0,0 +1,44 @@
+import logging
+import sys
+
+import zmq
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+if __name__ == '__main__':
+    context = zmq.Context()
+
+    frontend = context.socket(zmq.ROUTER)
+    backend = context.socket(zmq.DEALER)
+
+    logging.info('%s', dir(frontend))
+
+    frontend.bind('tcp://*:5559')
+    backend.bind('tcp://*:5560')
+
+    poller = zmq.Poller()
+    poller.register(frontend, zmq.POLLIN)
+    poller.register(backend, zmq.POLLIN)
+
+    logging.info('starting broker')
+    while True:
+        socks = poller.poll()
+        for sock, mask in socks:
+            if sock == frontend:
+                logging.info('forwarding request')
+
+                while True:
+                    message = sock.recv()
+                    more = sock.getsockopt(zmq.RCVMORE)
+                    backend.send(message, zmq.SNDMORE if more else 0)
+                    if more == 0:
+                        break
+            elif sock == backend:
+                logging.info('forwarding reply')
+                while True:
+                    message = sock.recv()
+                    more = sock.getsockopt(zmq.RCVMORE)
+                    frontend.send(message, zmq.SNDMORE if more else 0)
+                    if more == 0:
+                        break
---
+++ module_zmq/zmq_request_reply_extened/client.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+
+import zmq
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+if __name__ == '__main__':
+    context = zmq.Context()
+    # socket talk to server
+    requester = context.socket(zmq.REQ)
+    requester.connect('tcp://localhost:5559')
+
+    logging.info('start requesting')
+    for i in range(10):
+        logging.info('request %s', i)
+        requester.send(b'hello')
+        reply = requester.recv()
+        logging.info('received reply %s', reply)
