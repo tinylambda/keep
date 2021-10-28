9ac63b3195f28658eccdf1e80df6ff70d60f5847
Felix <tinylambda@gmail.com>
2021-10-28 11:48:23 +0800

    use etcd to watch keys

---
+++ module_aetcd3/aetcd3_watch_a_key.py
@@ -0,0 +1,27 @@
+import asyncio
+import logging
+import sys
+
+import aetcd3
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+async def main():
+    async with aetcd3.client() as client:
+        await client.put('foo', 'bar')
+        v = await client.get('foo')
+        logging.info('value of foo is %s', v)
+
+        events_iterator, cancel = await client.watch('foo')
+        event_times = 0
+        async for event in events_iterator:
+            logging.info('event is %s', event)
+            event_times += 1
+            if event_times > 5:
+                logging.info('cancelling event %s', event)
+                await cancel()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
