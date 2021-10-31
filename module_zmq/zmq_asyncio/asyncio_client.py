2eb5eb5cebe9cf5ca66159558febff8fd1298a42
Felix Pan <tinylambda@gmail.com>
2021-10-31 13:33:48 +0800

    zmq use asyncio

---
+++ module_zmq/zmq_asyncio/asyncio_client.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+import sys
+
+import zmq.asyncio
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+async def request(context: zmq.asyncio.Context, connect_to):
+    req_sock = context.socket(zmq.REQ)
+    req_sock.connect(connect_to)
+
+    for i in range(10):
+        await req_sock.send(b'hello')
+        reply = await req_sock.recv()
+        logging.info('received: %s', reply)
+
+
+if __name__ == '__main__':
+    ctx = zmq.asyncio.Context()
+    asyncio.run(request(ctx, 'tcp://localhost:5555'))
---
+++ module_zmq/zmq_asyncio/asyncio_server.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+import sys
+
+import zmq.asyncio
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+async def start_server(context: zmq.asyncio.Context, bind_to: str):
+    rep_sock = context.socket(zmq.REP)
+    rep_sock.bind(bind_to)
+
+    while True:
+        msg = await rep_sock.recv()
+        logging.info('received: %s', msg)
+        await rep_sock.send(b'world')
+
+
+if __name__ == '__main__':
+    ctx = zmq.asyncio.Context()
+    asyncio.run(start_server(ctx, 'tcp://*:5555'))
