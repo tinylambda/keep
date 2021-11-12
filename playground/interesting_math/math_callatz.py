fe9449983bcc70e4e16711aa0ca4fd448aab23af
Felix Pan <tinylambda@gmail.com>
2021-11-12 23:17:30 +0800

    find callatz sequence

---
+++ playground/interesting_math/math_callatz.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+
+import zmq
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+def find_callatz(n: int):
+    tries = 0
+    value_seq = []
+
+    while True:
+        tries += 1
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = (3 * n + 1) // 2
+
+        value_seq.append(n)
+        if n == 1:
+            break
+    return ','.join(map(str, value_seq))
+
+
+if __name__ == '__main__':
+    for num in range(1, 100):
+        logging.info(find_callatz(num))
