898d3b2dc17bebe498cbca38e2d088be70068103
Felix <tinylambda@gmail.com>
2021-12-02 16:36:25 +0800

    use complex type

---
+++ module_typing/typing_complex.py
@@ -0,0 +1,16 @@
+import logging
+import sys
+
+import attr
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+
+@attr.s
+class C:
+    complex = attr.ib(type=list[list[dict]])
+
+
+if __name__ == '__main__':
+    c = C(complex=[[{}, {}], [{}, {}]])
+    logging.info('%s', c)
