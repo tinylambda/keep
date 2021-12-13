4300331661ad2c9e89d15e6f476a61b69ab6dc5a
Felix <tinylambda@gmail.com>
2021-12-13 11:24:46 +0800

    sync code

--- module_sqlparse/sqlparse_extract_multisql.py
+++ module_sqlparse/sqlparse_extract_multisql.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     sql = 'select * from a;select * from b'
     logging.info('%s', sqlparse.split(sql))
 
-    sql = 'select col1, "\;go\;" from a;select col2 from b'
+    sql = 'select col1, "\;go\;" from a;select col2 from b;insert overwrite table result_table partition(a="100") select  col1 from tableC'
     logging.info('%s', sqlparse.split(sql))
