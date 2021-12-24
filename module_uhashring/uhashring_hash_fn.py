911acd260c8035eaab0bc3ad324ba91d7c764d10
Felix <tinylambda@gmail.com>
2021-12-24 16:50:33 +0800

    add uhashring

---
+++ module_uhashring/uhashring_hash_fn.py
@@ -0,0 +1,22 @@
+from uhashring import HashRing
+
+if __name__ == '__main__':
+    hr = HashRing(nodes=['node1', 'node2', 'node3'], hash_fn='ketama')
+    target_node = hr.get_node('coconut')
+    print(target_node)
+
+    hr.add_node('node4')
+    target_node = hr.get_node('coconut')
+    print(target_node)
+
+    hr.add_node('node5')
+    target_node = hr.get_node('coconut')
+    print(target_node)
+
+    hr.add_node('node6')
+    target_node = hr.get_node('coconut')
+    print(target_node)
+
+    hr.add_node('node7')
+    target_node = hr.get_node('coconut')
+    print(target_node)
---
+++ module_uhashring/uhashring_simple.py
@@ -0,0 +1,7 @@
+from uhashring import HashRing
+
+# create a consistent hash ring of 3 nodes of weight 1
+hr = HashRing(nodes=['node1', 'node2', 'node3'])
+
+# get the node name for the 'coconut' key
+target_node = hr.get_node('coconut')
