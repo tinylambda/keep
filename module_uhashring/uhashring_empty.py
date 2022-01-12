6c003099d681ba1383fc059b905480360db3e26d
Felix <tinylambda@gmail.com>
2022-01-12 15:56:22 +0800

    more on uhashring

---
+++ module_uhashring/uhashring_empty.py
@@ -0,0 +1,8 @@
+from uhashring import HashRing
+
+# create a consistent hash ring of 3 nodes of weight 1
+hr = HashRing(nodes=[])
+
+# get the node name for the 'coconut' key
+target_node = hr.get_node('coconut')
+print(target_node)  # is None
