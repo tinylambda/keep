c5e26d6893cf3ba30fa578ea01c1de05f95fd09c
Felix Pan <tinylambda@gmail.com>
2022-01-16 12:35:58 +0800

    more on sys

---
+++ module_sys/exception_handling/sys_excepthook.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def my_excepthook(type, value, traceback):
+    print('Unhandled error: ', type, value)
+
+
+sys.excepthook = my_excepthook
+
+print('Before exception')
+raise RuntimeError('This is the error message')
+print('After exception')
