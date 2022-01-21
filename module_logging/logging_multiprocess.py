f816d1b83ad41395e67d95bd1096b76a4080bb76
Felix Pan <tinylambda@gmail.com>
2022-01-21 20:11:44 +0800

    logging multiprocess test

---
+++ module_logging/logging_multiprocess.py
@@ -0,0 +1,22 @@
+import concurrent.futures
+import logging
+import logging.handlers
+import sys
+
+
+def do_log(n):
+    logger = logging.getLogger()
+    handler = logging.StreamHandler(stream=sys.stdout)
+    formatter = logging.Formatter(r"%(asctime)s %(name)s[%(process)d] [%(levelname)s] %(message)s")
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.DEBUG)
+    logger.handlers = [handler]
+    logger.setLevel(logging.DEBUG)
+
+    for i in range(1000):
+        logger.info('Hi %s', i)
+
+
+if __name__ == '__main__':
+    executor = concurrent.futures.ProcessPoolExecutor(max_workers=16)
+    results = executor.map(do_log, range(16))
