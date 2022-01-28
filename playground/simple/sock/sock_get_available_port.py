7280ec56c3f6794af1977df84583b29600f00f12
Felix Pan <tinylambda@gmail.com>
2022-01-28 14:55:32 +0800

    get available port

---
+++ playground/simple/sock/sock_get_available_port.py
@@ -0,0 +1,20 @@
+import socket
+
+
+def get_available_port():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind(("", 0))
+        return s.getsockname()[1]
+    finally:
+        s.close()
+
+
+if __name__ == '__main__':
+    ports = []
+    for _ in range(500):
+        port = get_available_port()
+        if port in ports:
+            print('Duplicated port !')
+            continue
+        ports.append(port)
