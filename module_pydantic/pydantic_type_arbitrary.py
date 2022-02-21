cd29a4b3427e37f776476e99d8b524b1af45d580
Felix Pan <tinylambda@gmail.com>
2022-02-21 23:22:44 +0800

    more on pydantic type

---
+++ module_pydantic/pydantic_type_arbitrary.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel, ValidationError
+
+
+class Pet:
+    def __init__(self, name: str):
+        self.name = name
+
+
+class Model(BaseModel):
+    pet: Pet
+    owner: str
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+if __name__ == '__main__':
+    pet = Pet(name='Hedwig')
+    m = Model(owner='Felix', pet=pet)
+    print(m)
+    print(m.pet)
+    print(m.pet.name)
+    print(type(m.pet))
+
+    try:
+        Model(owner='Felix', pet='Hewig')
+    except ValidationError as e:
+        print(e)
+
+    # Nothing in the instance of the arbitrary type is checked
+    # Here name probably should have been a str, but it's not validated
+    pet2 = Pet(name=42)
+    m2 = Model(owner='Felix', pet=pet2)
+    print(m2)
+    print(m2.pet)
+    print(m2.pet.name)
+    print(type(m2.pet))
---
+++ module_pydantic/pydantic_type_generic.py
@@ -0,0 +1,69 @@
+from typing import TypeVar, Generic
+
+from pydantic import ValidationError, BaseModel
+from pydantic.fields import ModelField
+
+AgedType = TypeVar('AgedType')
+QualityType = TypeVar('QualityType')
+
+
+class TastingModel(Generic[AgedType, QualityType]):
+    def __init__(self, name: str, aged: AgedType, quality: QualityType):
+        self.name = name
+        self.aged = aged
+        self.quality = quality
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v, field: ModelField):
+        if not isinstance(v, cls):
+            raise TypeError('Invalid value')
+        if not field.sub_fields:
+            return v
+        aged_f = field.sub_fields[0]
+        quality_f = field.sub_fields[1]
+        errors = []
+
+        valid_value, error = aged_f.validate(v.aged, {}, loc='aged')
+        if error:
+            errors.append(error)
+
+        valid_value, error = quality_f.validate(v.quality, {}, loc='quality')
+        if error:
+            errors.append(error)
+
+        if errors:
+            raise ValidationError(errors, cls)
+        return v
+
+
+class Model(BaseModel):
+    wine: TastingModel[int, float]
+    cheese: TastingModel[bool, str]
+    thing: TastingModel
+
+
+if __name__ == '__main__':
+    m = Model(
+        wine=TastingModel(name='Cabernet Sauvignon', aged=20, quality=85.6),
+        cheese=TastingModel(name='Gouda', aged=True, quality='Good'),
+        thing=TastingModel(name='Python', aged='not much', quality='Awesome')
+    )
+    print(m)
+    print(m.wine.aged)
+    print(m.wine.quality)
+
+    print(m.cheese.aged)
+    print(m.cheese.quality)
+
+    print(m.thing.aged)
+
+    try:
+        Model(wine=TastingModel(name='Merlot', aged=True, quality='Kinda good'),
+              cheese=TastingModel(name='Gouda', aged='yeah', quality=5),
+              thing=TastingModel(name='Python', aged='not much', quality='Awesome'))
+    except ValidationError as e:
+        print(e)
