88086789083fa4d6ac94bf32d677c6b0d6c6327d
Felix Pan <tinylambda@gmail.com>
2022-02-23 21:33:04 +0800

    pydantic simple validator

---
+++ module_pydantic/pydantic_validator_multi.py
@@ -0,0 +1,46 @@
+from typing import List
+
+from pydantic import BaseModel, validator, ValidationError
+
+
+class DemoModel(BaseModel):
+    square_numbers: List[int] = []
+    cube_numbers: List[int] = []
+
+    @validator('*', pre=True)
+    def split_str(cls, v):
+        if isinstance(v, str):
+            return v.split('|')
+        return v
+
+    @validator('cube_numbers', 'square_numbers')
+    def check_sum(cls, v):
+        if sum(v) > 42:
+            raise ValueError('sum of numbers greater than 42')
+        return v
+
+    @validator('square_numbers', each_item=True)
+    def check_squares(cls, v):
+        assert v ** 0.5 % 1 == 0, f'{v} is not a square number'
+        return v
+
+    @validator('cube_numbers', each_item=True)
+    def check_cubes(cls, v):
+        assert v ** (1 / 3) % 1 == 0, f'{v} is not a cubed number'
+        return v
+
+
+if __name__ == '__main__':
+    print(DemoModel(square_numbers=[1, 4, 9]))
+    print(DemoModel(square_numbers='1|4|9'))
+    print(DemoModel(square_numbers=[16], cube_numbers=[8, 27]))
+
+    try:
+        DemoModel(square_numbers=[1, 4, 12])
+    except ValidationError as e:
+        print(e)
+
+    try:
+        DemoModel(cube_numbers=[27, 27])
+    except ValidationError as e:
+        print(e)
---
+++ module_pydantic/pydantic_validator_simple.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel, validator, ValidationError
+
+
+class UserModel(BaseModel):
+    name: str
+    username: str
+    password1: str
+    password2: str
+
+    @validator('name')
+    def name_must_contain_space(cls, v):
+        if ' ' not in v:
+            raise ValueError('must contain a space')
+        return v.title()
+
+    @validator('password2')
+    def passwords_match(cls, v, values, **kwargs):
+        if 'password1' in values and v != values['password1']:
+            raise ValueError('passwords do not match')
+        return v
+
+    @validator('username')
+    def username_alphanumeric(cls, v):
+        assert v.isalnum(), 'must be alphanumeric'
+        return v
+
+
+if __name__ == '__main__':
+    user = UserModel(
+        name='Felix Pan',
+        username='tinylambda',
+        password1='abc123',
+        password2='abc123',
+    )
+    print(user)
+
+    try:
+        UserModel(
+            name='felix',
+            username='felix',
+            password1='abc123',
+            password2='abc456',
+        )
+    except ValidationError as e:
+        print(e)
+
+
