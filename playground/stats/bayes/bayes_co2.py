829a26fd7be36b2a0c345c7dec298e37813ec10e
Felix Pan <tinylambda@gmail.com>
2022-02-25 21:07:34 +0800

    more on stats

---
+++ playground/stats/bayes/bayes_co2.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+    data = np.genfromtxt('co2.csv', delimiter=',')
+    plt.plot(data[:, 0], data[:, 1])
+    plt.xlabel('$year$', fontsize=16)
+    plt.ylabel('$CO_2 (ppmv)$', fontsize=16)
+    plt.show()
+    
