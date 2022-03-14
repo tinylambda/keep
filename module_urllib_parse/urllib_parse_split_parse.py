b0f9848b9e57f36a8ea807eef9e9dd05e1be78d3
Felix Pan <tinylambda@gmail.com>
2022-03-14 17:06:36 +0800

    urlsplit, urlparse difference

---
+++ module_urllib_parse/urllib_parse_split_parse.py
@@ -0,0 +1,6 @@
+import urllib.parse
+
+if __name__ == '__main__':
+    url = 'http://example.com:8000/pa/th;param1=foo;param2=bar?name=val#frag'
+    print(urllib.parse.urlsplit(url))
+    print(urllib.parse.urlparse(url))
