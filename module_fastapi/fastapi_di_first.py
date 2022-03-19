daf1eae1e2ed36cd540b144d999b6e387384d776
Felix Pan <tinylambda@gmail.com>
2022-03-19 20:53:51 +0800

    fastapi di basics

---
+++ module_fastapi/fastapi_di_first.py
@@ -0,0 +1,46 @@
+"""
+"Dependency Injection" means, in programming, that there is a way for your code (in this case,
+ your path operation functions) to declare things that it requires to work and use: "dependencies".
+
+ And then, that system (in this case FastAPI) will take care of doing whatever is needed to provide
+ your code with those needed dependencies ("inject" the dependencies).
+
+ This is very useful when you need to:
+
+    Have shared logic (the same code logic again and again).
+    Share database connections.
+    Enforce security, authentication, role requirements, etc.
+    And many other things...
+ All these, while minimizing code repetition.
+"""
+from typing import Optional
+
+from fastapi import FastAPI, Depends
+
+app = FastAPI()
+
+
+async def common_parameters(q: Optional[str] = None, skip: int = 0, limit: int = 100):
+    return {'q': q, 'skip': skip, 'limit': limit}
+
+"""
+Whenever a new request arrives, FastAPI will take care of:
+
+    Calling your dependency ("dependable") function with the correct parameters.
+    Get the result from your function.
+    Assign that result to the parameter in your path operation function.
+
+"""
+
+
+@app.get('/items/')
+async def read_items(commons: dict = Depends(common_parameters)):
+    return commons
+
+
+@app.get('/users/')
+async def read_users(commons: dict = Depends(common_parameters)):
+    return commons
+
+
+# PYTHONPATH=module_fastapi uvicorn fastapi_di_first:app --reload
---
+++ module_fastapi/fastapi_di_sub.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+from fastapi import FastAPI, Depends, Cookie
+
+app = FastAPI()
+
+
+def query_extractor(q: Optional[str] = None):
+    return q
+
+
+def query_or_cookie_extractor(q: str = Depends(query_extractor), last_query: Optional[str] = Cookie(None)):
+    if not q:
+        return last_query
+    return q
+
+
+@app.get('/items/')
+async def read_query(query_or_default: str = Depends(query_or_cookie_extractor)):
+    return {'q_or_cookie': query_or_default}
+
+
+"""
+Using the same dependency multiple times¶
+
+If one of your dependencies is declared multiple times for the same path operation, for example, multiple dependencies 
+have a common sub-dependency, FastAPI will know to call that sub-dependency only once per request.
+
+And it will save the returned value in a "cache" and pass it to all the "dependants" that need it in that specific 
+request, instead of calling the dependency multiple times for the same request.
+
+In an advanced scenario where you know you need the dependency to be called at every step (possibly multiple times) in the same request instead of using the "cached" value, you can set the parameter use_cache=False when using Depends:
+"""
+
+# PYTHONPATH=module_fastapi uvicorn fastapi_di_sub:app --reload
