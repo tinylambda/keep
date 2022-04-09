10adeeb944b71acfb51f5b7235b70f7b32fb9104
Felix Pan <tinylambda@gmail.com>
2022-04-09 23:02:04 +0800

    sqlite3 basic operations

---
+++ module_sqlite3/sqlite3_argument_named.py
@@ -0,0 +1,21 @@
+import sqlite3
+import sys
+
+
+if __name__ == '__main__':
+    db_filename = 'todo.db'
+    project_name = sys.argv[1]
+
+    with sqlite3.connect(db_filename) as conn:
+        cursor = conn.cursor()
+
+        query = '''
+        select id, priority, details, status, deadline
+        from task
+        where project = :project_name
+        '''
+
+        cursor.execute(query, {'project_name': project_name})
+        for row in cursor.fetchall():
+            task_id, priority, details, status, deadline = row
+            print('{:2d} [{:d}] {:<25} [{:<8}] ({})'.format(task_id, priority, details, status, deadline))
---
+++ module_sqlite3/sqlite3_cursor_description.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+if __name__ == '__main__':
+    db_filename = 'todo.db'
+
+    with sqlite3.connect(db_filename) as conn:
+        cursor = conn.cursor()
+
+        cursor.execute('''
+        select *
+        from task
+        where project = 'pymotw'
+        ''')
+
+        print('Task table has these columns:')
+        for colinfo in cursor.description:
+            print(colinfo)
