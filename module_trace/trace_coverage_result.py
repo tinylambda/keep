76bdda2d1a233396da8820b3b726f8944342c6b7
Felix Pan <tinylambda@gmail.com>
2022-04-19 23:39:52 +0800

    add module trace

---
+++ module_trace/trace_coverage_result.py
@@ -0,0 +1,10 @@
+import trace
+from trace_example.recurse import recurse
+
+
+if __name__ == '__main__':
+    tracer = trace.Trace(count=True, trace=False)
+    tracer.runfunc(recurse, 2)
+
+    results = tracer.results()
+    results.write_results(coverdir='coverdir2')
---
+++ module_trace/trace_run.py
@@ -0,0 +1,7 @@
+import trace
+from trace_example.recurse import recurse
+
+
+if __name__ == '__main__':
+    tracer = trace.Trace(count=False, trace=True)
+    tracer.run('recurse(2)')
