183e318ca8b9d664435be318bc8423af00492c21
Felix Pan <tinylambda@gmail.com>
2022-04-24 21:25:26 +0800

    more on django ninja auth

--- module_django/django_ninja_simple/django_ninja_simple/api.py
+++ module_django/django_ninja_simple/django_ninja_simple/api.py
@@ -3,7 +3,7 @@ from typing import Optional, Any
 
 from django.http import HttpRequest
 from ninja import NinjaAPI, Schema, Path, Form
-from ninja.security import django_auth, HttpBearer
+from ninja.security import django_auth, HttpBearer, HttpBasicAuth
 
 
 class AuthBearer(HttpBearer):
@@ -16,6 +16,14 @@ class GlobalAuth(HttpBearer):
     def authenticate(self, request: HttpRequest, token: str) -> Optional[Any]:
         if token == 'supersecret':
             return token
+
+
+class BasicAuth(HttpBasicAuth):
+    def authenticate(
+        self, request: HttpRequest, username: str, password: str
+    ) -> Optional[Any]:
+        if username == 'admin' and password == 'secret':
+            return username
 
 
 api = NinjaAPI(csrf=True, auth=GlobalAuth())
@@ -60,7 +68,22 @@ def bearer(request):
     return {'token': request.auth}
 
 
-@api.post('/token', auth=None)
+@api.post('/token', auth=None)  # < overriding global auth
 def get_token(request, username: str = Form(...), password: str = Form(...)):
     if username == 'admin' and password == '123':
         return {'token': 'supersecret'}
+
+
+def ip_white_list(request):
+    if request.META['REMOTE_ADDR'] == '127.0.0.1':
+        return '8.8.8.8'
+
+
+@api.get('/ipwhitelist', auth=ip_white_list)
+def ipwhitelist(request):
+    return f'Authenticated client, IP = {request.auth}'
+
+
+@api.get('/basic', auth=BasicAuth())
+def basic(request):
+    return {'httpuser': request.auth}
