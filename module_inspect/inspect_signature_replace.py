b66a40426c84160810701fe7c6055f40532c9aff
Felix Pan <tinylambda@gmail.com>
2022-04-28 17:44:14 +0800

    signature replace

---
+++ module_inspect/inspect_signature_replace.py
@@ -0,0 +1,19 @@
+import inspect
+
+
+def func(self, request, a, b, c, d, *args, **kwargs):
+    pass
+
+
+if __name__ == '__main__':
+    skip_parameters = ['self', 'request']
+    sig_inspect = inspect.signature(func)
+    sig_parameter = []
+    for parameter in sig_inspect.parameters.values():
+        if parameter.name not in skip_parameters:
+            sig_parameter.append(parameter)
+
+    print(sig_inspect)
+    sig_replaced = sig_inspect.replace(parameters=sig_parameter)
+    print(sig_replaced)
+
