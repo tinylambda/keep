fd4bb3cd148ee54174c3c8ca3cd77aca76db2e02
Felix Pan <tinylambda@gmail.com>
2022-05-11 18:41:12 +0800

    some interesting games

---
+++ playground/gof.py
@@ -0,0 +1,100 @@
+from collections import namedtuple
+
+ALIVE = '*'
+EMPTY = '-'
+TICK = object()
+
+
+Query = namedtuple('Query', ('y', 'x'))
+Transition = namedtuple('Transition', ('y', 'x', 'state'))
+
+
+def count_neighbors(y, x):
+    n_ = yield Query(y + 1, x + 0)  # North
+    ne = yield Query(y + 1, x + 1)  # North East
+    e_ = yield Query(y + 0, x + 1)  # East
+    se = yield Query(y - 1, x + 1)  # South East
+    s_ = yield Query(y - 1, x + 0)  # South
+    sw = yield Query(y - 1, x - 1)  # South West
+    w_ = yield Query(y - 0, x - 1)  # West
+    nw = yield Query(y + 1, x - 1)  # North West
+
+    neighbor_states = [n_, ne, e_, se, s_, sw, w_, nw]
+    count = 0
+    for state in neighbor_states:
+        if state == ALIVE:
+            count += 1
+    return count
+
+
+def game_logic(state, neighbors):
+    if state == ALIVE:
+        if neighbors < 2:
+            return EMPTY
+        elif neighbors > 3:
+            return EMPTY
+    else:
+        if neighbors == 3:
+            return ALIVE
+    return state
+
+
+def step_cell(y, x):
+    state = yield Query(y, x)
+    neighbors = yield from count_neighbors(y, x)
+    next_state = game_logic(state, neighbors)
+    yield Transition(y, x, next_state)
+
+
+class Grid:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        self.rows = []
+        for _ in range(self.height):
+            self.rows.append([EMPTY] * self.width)
+
+    def query(self, y, x):
+        return self.rows[y % self.height][x % self.width]
+
+    def assign(self, y, x, state):
+        self.rows[y % self.height][x % self.width] = state
+
+    def __str__(self):
+        line_fmt = '{}' * self.width
+        lines = [line_fmt.format(*item) for item in self.rows]
+        return '\n'.join(lines)
+
+
+def simulate(height: int, width: int):
+    while True:
+        for y in range(height):
+            for x in range(width):
+                yield from step_cell(y, x)
+        yield TICK
+
+
+def live_a_generation(grid: Grid, sim):
+    progeny = Grid(grid.height, grid.width)
+    item = next(sim)
+    while item is not TICK:
+        if isinstance(item, Query):
+            state = grid.query(item.y, item.x)
+            item = sim.send(state)
+        else:
+            progeny.assign(item.y, item.x, item.state)
+            item = next(sim)
+    return progeny
+
+
+if __name__ == '__main__':
+    grid = Grid(5, 9)
+    grid.assign(0, 3, ALIVE)
+    grid.assign(0, 4, ALIVE)
+    grid.assign(1, 4, ALIVE)
+    grid.assign(1, 5, ALIVE)
+    sim = simulate(grid.height, grid.width)
+    for i in range(50):
+        print(grid)
+        grid = live_a_generation(grid, sim)
+        print('O' * grid.width)
---
+++ playground/gof2.py
@@ -0,0 +1,105 @@
+import copy
+
+
+class State:
+    DEAD = 'X'
+    LIVE = 'O'
+
+    def __init__(self, row_num: int, column_num: int):
+        self.row_num = row_num
+        self.column_num = column_num
+
+        self._state = []
+        for _ in range(row_num):
+            self._state.append([self.DEAD for _ in range(column_num)])
+
+    def __str__(self):
+        # print the overall state
+        line_fmt = '{}' * self.column_num
+        lines = [line_fmt.format(*line_state) for line_state in self._state]
+        return '\n'.join(lines)
+
+    def _ensure_index(self, row_index, column_index):
+        # handle index overflow including negative index
+        _row_index = row_index % self.row_num
+        _column_index = column_index % self.column_num
+        return _row_index, _column_index
+
+    def state_at(self, row_index, column_index):
+        _row_index, _column_index = self._ensure_index(row_index, column_index)
+        return self._state[_row_index][_column_index]
+
+    def set_state_at(self, row_index, column_index, state):
+        _row_index, _column_index = self._ensure_index(row_index, column_index)
+        self._state[_row_index][_column_index] = state
+
+    def forward_at(self, row_index, column_index):
+        _row_index, _column_index = self._ensure_index(row_index, column_index)
+        current_state = self.state_at(_row_index, _column_index)
+        count_alive_neighbors = self.count_alive_neighbors_at(_row_index, _column_index)
+
+        new_state = copy.deepcopy(self._state)
+        # how to change state
+        if current_state == self.LIVE:
+            if count_alive_neighbors < 2 or count_alive_neighbors > 3:
+                new_state[_row_index][_column_index] = self.DEAD
+        else:
+            if count_alive_neighbors == 3:
+                new_state[_row_index][_column_index] = self.LIVE
+
+        self._state = new_state
+
+    def refresh(self):
+        for row_index in range(self.row_num):
+            for column_index in range(self.column_num):
+                self.forward_at(row_index, column_index)
+
+    def count_alive_neighbors_at(self, row_index, column_index):
+        top_coord = (row_index - 1, column_index)
+        top_right_coord = (row_index - 1, column_index + 1)
+        right_coord = (row_index, column_index + 1)
+        bottom_right_coord = (row_index + 1, column_index + 1)
+        bottom_coord = (row_index + 1, column_index)
+        bottom_left_coord = (row_index + 1, column_index - 1)
+        left_coord = (row_index, column_index - 1)
+        top_left_coord = (row_index + 1, column_index - 1)
+
+        coord_list = [
+            top_coord,
+            top_right_coord,
+            right_coord,
+            bottom_right_coord,
+            bottom_coord,
+            bottom_left_coord,
+            left_coord,
+            top_left_coord,
+        ]
+
+        neighbor_states = [self.state_at(*arg) for arg in coord_list]
+        return len([item for item in neighbor_states if item == self.LIVE])
+
+
+if __name__ == '__main__':
+    s = State(4, 4)
+    s.set_state_at(1, 1, s.LIVE)
+    s.set_state_at(0, 0, s.LIVE)
+    s.set_state_at(0, 1, s.LIVE)
+    s.set_state_at(0, 2, s.LIVE)
+    s.set_state_at(2, 1, s.LIVE)
+    print(s)
+
+    print('*' * 64)
+    s.refresh()
+    print(s)
+
+    print('*' * 64)
+    s.refresh()
+    print(s)
+
+    print('*' * 64)
+    s.refresh()
+    print(s)
+
+    print('*' * 64)
+    s.refresh()
+    print(s)
