05e10825b336fc3ed691aef38baf245abe781f56
Felix Pan <tinylambda@gmail.com>
2022-05-13 23:39:32 +0800

    readline basic usage

---
+++ module_readline/readline_completer.py
@@ -0,0 +1,35 @@
+import readline
+
+
+class SimpleCompleter:
+    def __init__(self, options):
+        self.options = sorted(options)
+
+    def complete(self, text, state):
+        response = None
+        if state == 0:
+            # This is the first time for this text
+            # so build a match first
+            if text:
+                self.matches = [s for s in self.options if s and s.startswith(text)]
+            else:
+                self.matches = self.options[:]
+        try:
+            response = self.matches[state]
+        except IndexError:
+            response = None
+        return response
+
+
+def input_loop():
+    line = ''
+    while line != 'stop':
+        line = input('Prompt ("stop" to quit):')
+        print('Dispatch {}'.format(line))
+
+
+OPTIONS = ['start', 'stop', 'list', 'print']
+readline.set_completer(SimpleCompleter(OPTIONS).complete)
+readline.parse_and_bind('tab: complete')
+
+input_loop()
---
+++ module_readline/readline_parse_and_bind.py
@@ -0,0 +1,12 @@
+import readline
+
+
+readline.parse_and_bind('tab: complete')
+readline.parse_and_bind('set editing-mode vi')
+
+if __name__ == '__main__':
+    while True:
+        line = input('Prompt ("stop" to quit):')
+        if line == 'stop':
+            break
+        print('Entered: {!r}'.format(line))
