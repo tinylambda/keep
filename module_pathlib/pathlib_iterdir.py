5adec861d79a35f40779ed8361778a9e63f246e2
Felix Pan <tinylambda@gmail.com>
2022-06-04 01:55:24 +0800

    pathlib output more info

--- module_pathlib/pathlib_iterdir.py
+++ module_pathlib/pathlib_iterdir.py
@@ -1,9 +1,8 @@
 import pathlib
 
-
-p = pathlib.Path("/home/felix/PycharmProjects/keep/")
+p = pathlib.Path("/tmp")
 
 # iterdir() is a generator, yielding a new Path instance for each item in the containing directory.
 # not recursive !
 for f in p.iterdir():
-    print(f)
+    print(f, type(f), f.is_file())
