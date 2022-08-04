bb659263dfd4539d64a1f2d73a083693ea539c63
Felix Pan <tinylambda@gmail.com>
2022-08-04 15:23:20 +0800

    data fix script

--- module_enum/enum_intflag.py
+++ module_enum/enum_intflag.py
@@ -13,3 +13,20 @@ if __name__ == "__main__":
 
     types = WorkType.Design | WorkType.Construction
     print(WorkType.Design in types)
+
+    zero_type = WorkType(0)
+    print(zero_type)
+
+    zero_type |= WorkType.Design
+    print(zero_type)
+
+    zero_type |= WorkType.Construction
+    print(zero_type)
+
+    print(type(zero_type))
+
+    # This works too
+    all_types = WorkType(7)
+    print(WorkType.Design in all_types)
+    print(WorkType.Construction in all_types)
+    print(WorkType.Others in all_types)
