6a2e37b509113da29a6077a09a367fb7a9f853a7
Felix Pan <tinylambda@gmail.com>
2022-09-06 15:38:21 +0800

    more test on field

---
+++ module_pydantic/pydantic_field_extra.py
@@ -0,0 +1,20 @@
+import pprint
+
+from pydantic import BaseModel, Field
+
+
+class User(BaseModel):
+    f: str = Field(newarg=100)
+
+
+if __name__ == "__main__":
+    user = User(f="hello")
+    print(user)
+    pprint.pprint(dir(user))
+    print(user.__fields__)
+    field_f = user.__fields__["f"]
+    pprint.pprint(dir(field_f))
+    print(field_f.field_info)
+
+    field_info = field_f.field_info
+    print(field_info.extra)
