e7a713af09c0376ee9a028390b3783954691d256
Felix Pan <tinylambda@gmail.com>
2022-10-11 18:25:18 +0800

    add testing code

--- books/pc/ch8/ch8_17_2_2.py
+++ books/pc/ch8/ch8_17_2_2.py
@@ -8,10 +8,17 @@ class Date:
         self.day = day
 
     @classmethod
-    def today(cls):
+    def today(cls) -> "Date":
         d = cls.__new__(cls)
         t = time.localtime()
         d.year = t.tm_year
         d.month = t.tm_mon
         d.day = t.tm_mday
         return d
+
+
+if __name__ == "__main__":
+    today: Date = Date.today()
+    print(today.year)
+    print(today.month)
+    print(today.day)
